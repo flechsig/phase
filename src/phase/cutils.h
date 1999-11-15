87332143a0e189d2b3de89820a93f285b6179bff
flechsig <>
1999-11-15 07:06:20 +0000

    PrependEnv

--- src/phase/cutils.h
+++ src/phase/cutils.h
@@ -1,24 +1,13 @@
 /*  File      : /home/pss060/sls/flechsig/phase/src/phase/cutils.h */
 /*  Date      : <28 Oct 99 12:24:49 flechsig>  */
-/*  Time-stamp: <02 Nov 99 10:00:01 flechsig>  */
+/*  Time-stamp: <15 Nov 99 08:06:03 flechsig>  */
 /*  Author    : Flechsig Uwe OVGA/203a 4535, flechsig@psi.ch */
-
-/*  File      : /home/vms/flechsig/vms/phas/phasec/cutils.h */
-/*  Date      : <23 Apr 98 14:11:42 flechsig>  */
-/*  Time-stamp: <12 Oct 98 15:57:40 flechsig>  */
-/*  Author    : Uwe Flechsig, flechsig@exp.bessy.de */
-
-/* Datei: USERDISK_3:[FLECHSIG.PHASE.PHASEC]CUTILS.H           */
-/* Datum: 21.JUL.1994                                          */
-/* Stand: 17-FEB-1997                                          */
-/* Autor: FLECHSIG, BESSY Berlin                               */
 
 #ifndef CUTILS_H
 #define CUTILS_H    
 
 #define max(a,b) (((a) > (b)) ? (a) : (b))
 #define min(a,b) (((a) < (b)) ? (a) : (b))
-
 
 #define		FASTNULL		1.0e-15
 
@@ -28,12 +17,13 @@
 
    typedef struct dsc$descriptor_s FString;         /* FORTRAN- String */
 #else                   /* linux */
-   #define 	logfilename 		"/tmp/PHASEuser.log" 
+   #define 	logfilename 		"/tmp/phaseuser.log" 
    typedef      char FString; 
 #endif                  /*  VMS  */
 
 char    *delversion(char *); /* entfernt Versionsnummer von VMS- Filenamen */
 char    *FnameBody(char *);  /* holt Rumpf von VMS- - Filenamen            */
+char    *PrependEnv(char* , char *);
 void 	 beep(int); 
 void	 CheckUser(char *, char *); 
 int 	 CheckFileHeader(FILE *, char *);   
