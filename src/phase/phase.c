cfa2dd1ecbb2a427ce4c2215ad7feb5f1a183942
flechsig <>
1999-12-20 12:57:34 +0000

    VMS define

--- src/phase/phase.c
+++ src/phase/phase.c
@@ -1,6 +1,6 @@
 /*  File      : /home/pss060/sls/flechsig/phase/src/phase/phase.c */
 /*  Date      : <28 Oct 99 10:02:31 flechsig>  */
-/*  Time-stamp: <20 Dec 99 12:49:43 flechsig>  */
+/*  Time-stamp: <20 Dec 99 13:57:20 flechsig>  */
 /*  Author    : Flechsig Uwe OVGA/203a 4535, flechsig@psi.ch */
 
 #include <stdio.h>                    /* For printf and so on. */
@@ -48,7 +48,7 @@ unsigned int main(argc, argv)
 	printf("\nphase.c: environment variable %s not defined- exit\n",
 	       PHASE_HOME);
 	printf("example for unix tcsh: setenv %s ~/phase\n", PHASE_HOME);
-	printf("example for VMS: define %s [myname.phase\n", PHASE_HOME);
+	printf("example for VMS: define %s \"[myname.phase\"\n", PHASE_HOME);
 	exit(-1);
       }
 
