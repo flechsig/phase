ae3d75e7c2678504f02dc7dc8db013d659e1b006
flechsig <>
2003-10-27 13:11:41 +0000

    new version

--- src/phase/version.h
+++ src/phase/version.h
@@ -1,6 +1,6 @@
 /*   File      : /afs/psi.ch/user/f/flechsig/phase/src/phase/version.h */
 /*   Date      : <25 Jun 02 07:56:56 flechsig>  */
-/*   Time-stamp: <25 Jun 02 07:57:13 flechsig>  */
+/*   Time-stamp: <27 Oct 03 14:11:34 flechsig>  */
 /*   Author    : Uwe Flechsig, flechsig@psi.ch */
 
 /*   $Source$  */
@@ -10,8 +10,8 @@
 
 #ifndef VERSION_H
   #define VERSION_H
-  #define              VERSION_STRING        "1.13" 
-  #define              VERSION_DATE          "25-Jun-2002"
+  #define              VERSION_STRING        "1.14" 
+  #define              VERSION_DATE          "27-Oct-2003"
 #endif    /* VERSION_H */   
 
 /* end version.h */
