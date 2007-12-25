9114e0952075ba246a7db29ddf40c1b5a14f0eab
flechsig <>
2007-12-25 11:06:40 +0000

    make it save also with the posix version of basename

--- src/phase/error.c
+++ src/phase/error.c
@@ -1,6 +1,6 @@
 /*  File      : /afs/psi.ch/user/f/flechsig/phase/src/phase/error.c */
 /*  Date      : <26 Oct 04 13:49:18 flechsig>  */
-/*  Time-stamp: <06 Dec 07 18:32:38 flechsig>  */
+/*  Time-stamp: <25 Dec 07 11:59:33 flechsig>  */
 /*  Author    : Uwe Flechsig, flechsig@psi.ch */
 
 /*  $Source$  */
@@ -53,11 +53,15 @@ sic_fatal (const char *message)
 
 const char *program_name = NULL;
 
-void
-set_program_name (const char *path)
+/* failed with the posix version of basename */
+/* UF 25.12.07 apply basename to a copy      */
+void set_program_name (const char *path)
 {
+  char *pathcopy;
+  
+  pathcopy= xstrdup(path);
   /*  if (!program_name) */
-    program_name = xstrdup ((const char *)basename (path));
+  program_name = (const char *)basename(pathcopy);
 }
 
 
