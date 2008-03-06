9bf075e85b8f3ef1e3f2c5a9895ac39cacdd8431
leitner <>
2008-03-06 16:09:37 +0000

    removed all files, temporary

--- src/phase4idl/hhg_def.h
+++
@@ -1,9 +0,0 @@
-
-#ifndef __HHG_DEF
-#define __HHG_DEF
-
-#define MaxHarmonics 256
-
-
-#endif 
-/* end hhg_def.h */
--- src/phase4idl/test_phase.f
+++
@@ -1,7 +0,0 @@
-ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc77
-
-	include '/home/leitner/hhg/test_hhg.f'
-c	include '/home/leitner/fp/test_phase_gaussetc.f'
-c     include '/home/leitner/fp/benchmark.f'  
-
-ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc77
