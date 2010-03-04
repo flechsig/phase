b72b09a490190988efd7ea8d5ded06f7e0660f4c
gerhardt <>
2010-03-04 14:23:30 +0000

    phase_struct.h changed MaxGridSize to 2048  	Constants.h pha4idlFORTRANdefines.h.f phaSrc4Drift.F  	phaSrc4WFGauss.F

--- src/phase4idl/pha4idlFORTRANdefines.h.f
+++ src/phase4idl/pha4idlFORTRANdefines.h.f
@@ -3,6 +3,6 @@ cc /// pha4idlFORTRANdefines.h.f
 
 
 cc MaxDim is the Dimension of the fieldsw in the src4-structure
-#define MaxDim 1024
+#define MaxDim 2048
 cc BigMaxDim is for internal temporary fields
-#define BigMaxDim 1024
+#define BigMaxDim 2048 
