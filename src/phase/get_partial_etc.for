a7f4b8749f1b07dcbee4330ae22f50220dace4e3
bahrdt <>
2011-05-17 13:27:51 +0000

    verschiedenes

--- src/phase/get_partial_etc.for
+++ src/phase/get_partial_etc.for
@@ -1,16 +1,27 @@
 c---------------------------------------------------------------
-	subroutine get_partial_derivatives(dfdw,dfdl,
-     &             d2fdw2,d2fdwdl,d2fdl2,d3fdw3,iord)
+	subroutine get_partial_derivatives(opl4,dfdwmdln,iord)
 c---------------------------------------------------------------
+c
+c	Input:
+c		opl4(w,l,y,z)
+c
+c	intermediate variable:
+c		dfdwmdln0(w,l,y,z,m,n)
+c
+c	dann replace w,l 
+c
+c	dann
+c	Output:	dfdwmdln(y,z,dy,dz,m,n)
+c
+c----------------------------------------------------------------
 
 	implicit real*8(a-h,o-z)
 
-	dimension dfdw(0:7,0:7,0:7,0:7,0:7,0:7),
-     &    	  dfdl(0:7,0:7,0:7,0:7,0:7,0:7),
-     &		  d2fdw2(0:7,0:7,0:7,0:7,0:7,0:7),
-     &		  d2fdwdl(0:7,0:7,0:7,0:7,0:7,0:7),
-     &		  d3fdw3(0:7,0:7,0:7,0:7,0:7,0:7),
-     &		  d2fdl2(0:7,0:7,0:7,0:7,0:7,0:7)
+	dimension opl4(0:7,0:7,0:7,0:7)
+	dimension dfdwmdln0(0:7,0:7,0:7,0:7,0:7,0:7)
+	dimension dfdwmdln(0:7,0:7,0:7,0:7,0:7,0:7)
+
+c---------------------------------------------------------------
 
 	do i=0,iord
 	do j=0,iord-i
@@ -18,10 +29,7 @@ c---------------------------------------------------------------
 	do l=0,iord-i-j-k
 	do m=0,iord-i-j-k-l
 	do n=0,iord-i-j-k-l-m
-	  d2fdw2(i,j,k,l,m,n)=0.d0
-	  d3fdw3(i,j,k,l,m,n)=0.d0
-	  d2fdwdl(i,j,k,l,m,n)=0.d0
-	  d2fdl2(i,j,k,l,m,n)=0.d0
+	  dfdwmdln0(i,j,k,l,m,n)=0.d0
 	enddo
 	enddo
 	enddo
@@ -35,7 +43,7 @@ c---------------------------------------------------------------
 	do l=0,iord-i-j-k
 	do m=0,iord-i-j-k-l
 	do n=0,iord-i-j-k-l-m
-	  d2fdw2(i,j,k,l,m,n)=dflotj(i+1)*dfdw(i+1,j,k,l,m,n)
+	  dfdwmdln0(i,j,k,l,m,n)=dflotj(i+1)*dfdw(i+1,j,k,l,m,n)
 	enddo
 	enddo
 	enddo
@@ -92,6 +100,10 @@ c---------------------------------------------------------------
 c--------------------------------------------------------
 	subroutine get_dydz(a,g,eq27,eq28,iord)
 c--------------------------------------------------------
+c
+c	Input:	a, g, iord
+c	Output:	eq27(y,z,dy,dz,w,l) und eq28(y,z,dy,dz,w,l)
+c		daraus werden im Weiteren wc und xlc berechnet
 c
 c	i: y
 c	j: z
@@ -184,13 +196,19 @@ c-------- eq28
 	call Tay_inv_6(eq28dn,eq28dni,iord)
 	call Tay_mult_6(eq28n,eq28dni,eq28,iord)
 
+	eq27(0,0,1,0,0,0)=eq27(0,0,1,0,0,0)-1.d0
+	eq28(0,0,0,1,0,0)=eq28(0,0,0,1,0,0)-1.d0
+
 	return
 	end
 
 c--------------------------------------------------------
-	subroutine get_dypdzp(a1,g,wca,xlca,
-     &      ypca1,zpca1,eq33,eq34,iord)
+	subroutine get_dypdzp(a,g,wc,xlc,
+     &      ypc1,zpc1,dypc,dzpc,iord)
 c--------------------------------------------------------
+c
+c	Input:	a,g,wc,xlc,ypc1,zpc1,iord
+c	Output:	dypc,dzpc
 c
 c	i: y
 c	j: z
@@ -210,6 +228,7 @@ c--------------------------------------------------------
 	end structure
 	record /geometryst/ g
 
+	dimension a(0:8,0:8)
 	dimension a1(0:7,0:7,0:7,0:7)
 
 	dimension eq33(0:7,0:7,0:7,0:7)
@@ -221,20 +240,20 @@ c--------------------------------------------------------
 	dimension eq34dn(0:7,0:7,0:7,0:7)
 	dimension eq34dni(0:7,0:7,0:7,0:7)
 
-	dimension wca(0:7,0:7,0:7,0:7),
-     &		  xlca(0:7,0:7,0:7,0:7),
-     &            ypca1(0:7,0:7,0:7,0:7),
-     &            zpca1(0:7,0:7,0:7,0:7)
+	dimension wc(0:7,0:7,0:7,0:7),
+     &		  xlc(0:7,0:7,0:7,0:7),
+     &            ypc1(0:7,0:7,0:7,0:7),
+     &            zpc1(0:7,0:7,0:7,0:7)
 
 c------------------ replace variables
 c
-c	0)	wca(y,z,dy,dz) and xlca(y,z,dy,dz) are known
+c	0)	wc(y,z,dy,dz) and xlc(y,z,dy,dz) are known
 c
-c	1)	replace w and l in u getting
+c	1)	replace w and l in u(w,l) getting
 c		a1(y,z,dy,dz)
-	call replace_wl_in_u(a,wca,xlca,a1,iord)
+	call replace_wl_in_u(a,wc,xlc,a1,iord)
 c
-c	2) 	then, evaluate the quotient getting dyp and dzp
+c	2) 	then, evaluate the quotient getting dypc and dzpc
 c
 c-----------------------------------------------------------------
 	
@@ -258,7 +277,7 @@ c------- eq33 nominator
 	do k=0,iord-i-j
 	do l=0,iord-i-j-k
 	  eq33n(i,j,k,l)=a1(i,j,k,l)*g.sinb
-     &         -wca(i,j,k,l)*g.cosb+ypca1(i,j,k,l)
+     &         -wc(i,j,k,l)*g.cosb+ypc1(i,j,k,l)
 	enddo
 	enddo
 	enddo
@@ -270,7 +289,7 @@ c------- eq33 denominator
 	do k=0,iord-i-j
 	do l=0,iord-i-j-k
 	  eq33dn(i,j,k,l)=-a1(i,j,k,l)*g.cosb
-     &         -wca(i,j,k,l)*g.sinb
+     &         -wc(i,j,k,l)*g.sinb
 	enddo
 	enddo
 	enddo
@@ -280,7 +299,7 @@ c------- eq33 denominator
 	
 c-------- eq33
 	call Tay_inv_4(eq33dn,eq33dni,iord)
-	call Tay_mult_4(eq33n,eq33dni,eq33,iord)
+	call Tay_mult_4(eq33n,eq33dni,dypc,iord)
 
 c------- eq34
 c------- eq34 nominator
@@ -288,7 +307,7 @@ c------- eq34 nominator
 	do j=0,iord-i
 	do k=0,iord-i-j
 	do l=0,iord-i-j-k
-	  eq34n(i,j,k,l)=xlca(i,j,k,l)-zpca1(i,j,k,l)
+	  eq34n(i,j,k,l)=xlc(i,j,k,l)-zpc1(i,j,k,l)
 	enddo
 	enddo
 	enddo
@@ -299,7 +318,7 @@ c------- eq34 denominator
 	do j=0,iord-i
 	do k=0,iord-i-j
 	do l=0,iord-i-j-k
-	  eq34dn(i,j,k,l)=-wca(i,j,k,l)*g.sinb
+	  eq34dn(i,j,k,l)=-wc(i,j,k,l)*g.sinb
      &                    -a1(i,j,k,l)*g.cosb
 	enddo
 	enddo
@@ -310,7 +329,7 @@ c------- eq34 denominator
 	
 c-------- eq34
 	call Tay_inv_4(eq34dn,eq34dni,iord)
-	call Tay_mult_4(eq34n,eq34dni,eq34,iord)
+	call Tay_mult_4(eq34n,eq34dni,dzpc,iord)
 
 	return
 	end
