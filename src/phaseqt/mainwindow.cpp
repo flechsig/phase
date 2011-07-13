3838cd7910e0a674fc2dae6dfa51b8e86cb0c0a4
flechsig <>
2011-07-13 21:59:36 +0000

    *** empty log message ***

--- src/phaseqt/mainwindow.cpp
+++ src/phaseqt/mainwindow.cpp
@@ -1,6 +1,6 @@
 //  File      : /afs/psi.ch/user/f/flechsig/phase/src/qtgui/mainwindow.cpp
 //  Date      : <31 May 11 17:02:14 flechsig> 
-//  Time-stamp: <2011-07-13 23:53:15 flechsig> 
+//  Time-stamp: <2011-07-13 23:59:19 flechsig> 
 //  Author    : Uwe Flechsig, uwe.flechsig&#64;psi.&#99;&#104;
 
 //  $Source$ 
@@ -1880,9 +1880,9 @@ void MainWindow::parameterUpdate(int pos, char *text)
   switch (pos)
     {
     case 0:
-      scanned= sscanf(text, "%d", &this->BLOptions.ifl.iord);
-      if (scanned == EOF) this->BLOptions.ifl.iord= 4; // default
-      sprintf(buffer, "%d : %s", this->BLOptions.ifl.iord, "(epsilon) for Newton routine (1e-4)");
+      scanned= sscanf(text, "%lf", &this->BLOptions.epsilon);
+      if ((scanned == EOF) || (scanned == 0)) this->BLOptions.epsilon= 1e-4; // default
+      sprintf(buffer, "%5lg : %s", this->BLOptions.epsilon, "(epsilon) for Newton routine (1e-4)");
       break;
     case 1:
       scanned= sscanf(text, "%d", &this->BLOptions.ifl.iord);
