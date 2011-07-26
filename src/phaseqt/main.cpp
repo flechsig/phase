c6f550a539b00c0ba2e18ddea453bb3a88009187
flechsig <>
2011-07-26 10:12:37 +0000

    *** empty log message ***

--- src/phaseqt/main.cpp
+++ src/phaseqt/main.cpp
@@ -1,6 +1,6 @@
 //  File      : /afs/psi.ch/user/f/flechsig/phase/src/qtgui/main.cpp
 //  Date      : <31 May 11 16:51:36 flechsig> 
-//  Time-stamp: <27 Jun 11 15:55:36 flechsig> 
+//  Time-stamp: <26 Jul 11 12:12:22 flechsig> 
 //  Author    : Uwe Flechsig, uwe.flechsig&#64;psi.&#99;&#104;
 
 //  $Source$ 
@@ -12,12 +12,12 @@
 #include <QApplication>
 
 #include "mainwindow.h"
-#include "qtphase.h"
+#include "phaseqt.h"
 
 int main(int argc, char *argv[])
 {
   QApplication app(argc, argv);
-  Q_INIT_RESOURCE(qtgui);
+  Q_INIT_RESOURCE(phaseqt);
   MainWindow mainWin;
   mainWin.show();
   return app.exec();
