0134740320730aa31bfed7f65b22d4fa97eec567
flechsig <>
2011-07-29 12:03:33 +0000

    *** empty log message ***

---
+++ src/phaseqt/optiinput.cpp
@@ -0,0 +1,20 @@
+//  File      : /afs/psi.ch/user/f/flechsig/phase/src/phaseqt/optiinput.cpp
+//  Date      : <29 Jul 11 13:55:53 flechsig> 
+//  Time-stamp: <29 Jul 11 14:02:41 flechsig> 
+//  Author    : Uwe Flechsig, uwe.flechsig&#64;psi.&#99;&#104;
+
+//  $Source$ 
+//  $Date$
+//  $Revision$ 
+//  $Author$ 
+
+#include <QtGui>
+#include "optiinput.h"
+
+// constructor
+OptiInput::OptiInput(struct BeamlineType *parent)
+{
+
+  printf("OptiInput: constructor called\n");
+}
+// end
