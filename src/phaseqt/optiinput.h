cd51d9e4609cb3cbc1a78d63ec30e806ec93d2dc
flechsig <>
2011-07-29 12:00:16 +0000

    new

---
+++ src/phaseqt/optiinput.h
@@ -0,0 +1,29 @@
+/*  File      : /afs/psi.ch/user/f/flechsig/phase/src/phaseqt/optiinput.h */
+/*  Date      : <29 Jul 11 13:56:13 flechsig>  */
+/*  Time-stamp: <29 Jul 11 14:00:08 flechsig>  */
+/*  Author    : Uwe Flechsig, uwe.flechsig&#64;psi.&#99;&#104; */
+
+/*  $Source$  */
+/*  $Date$ */
+/*  $Revision$  */
+/*  $Author$  */
+
+#ifndef OPTIINPUT_H
+#define OPTIINPUT_H
+
+#include <QtGui>
+
+#include "phaseqt.h"
+
+class SingleRay : public QWidget, public QtPhase
+{
+    Q_OBJECT
+
+public:
+    QWidget     *optiBox;
+
+private slots:
+private:
+};
+#endif
+// end
