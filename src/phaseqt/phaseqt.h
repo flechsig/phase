402140024f5058453dc8562a6d99c7338fbb1f73
flechsig <>
2011-09-22 16:30:49 +0000

    *** empty log message ***

--- src/phaseqt/phaseqt.h
+++ src/phaseqt/phaseqt.h
@@ -44,43 +44,43 @@ class PhaseQt : public PHASEset, public BeamlineType
 {
   //  char my_global_rundir[MaxPathLength];
 public:
-  PhaseQt();
+  PhaseQt();   // constructor
   // add here member functions to access the structs PHASEset and BeamlineType
+  // wrapper funktions I call my... and define them here
+
   void initSet(const char *);
   void printSet();
   void initBeamline();
   struct BeamlineType *myBeamline();
-  struct OptionsType *myOptions();
-  struct PHASEset *myPHASEset(); 
+  struct OptionsType  *myOptions();
+  struct PHASEset     *myPHASEset(); 
   void myAllocRTSource() { AllocRTSource(this); }
   void myBatchMode(int cmode, int selected) { BatchMode(this, this, cmode, selected); }
   void myBuildBeamline() { BuildBeamline(this); }
   void myDefGeometryC (struct gdatset *x, struct geometrytype *gout) { DefGeometryC(x, gout); }
   void myDefMirrorC (struct mdatset *x, struct mirrortype *a, 
-		int etype, double theta, int lREDUCE_maps) { DefMirrorC(x, a, 
-		etype, theta, lREDUCE_maps); }
+		     int etype, double theta, int lREDUCE_maps) { 
+                     DefMirrorC(x, a, etype, theta, lREDUCE_maps); }
   void myFootprint(unsigned int enummer) { Footprint(this, enummer); }
-
   void myGetPHASE(char *name) { GetPHASE(this, name); }
   void myMakeMapandMatrix(struct ElementType *listpt) { MakeMapandMatrix(listpt, this); }
   void myMakeRTSource() { MakeRTSource(this, this);  }
-  
-  int  myProcComandLine(int argc, char *argv[], int *cmode, int *selected) { return ProcComandLine(this, argc, argv, cmode, selected); }
+  int  myProcComandLine(int argc, char *argv[], int *cmode, int *selected) { 
+                        return ProcComandLine(this, argc, argv, cmode, selected); }
   void myPutPHASE(char *name) { PutPHASE(this, name); }
-  int myReadBLFile(char *name) { return ReadBLFile(name, this); }
+  int  myReadBLFile(char *name) { return ReadBLFile(name, this); }
   void myReAllocResult(int newtype, int dim1, int dim2)   { ReAllocResult(this, newtype, dim1, dim2); }
   void myRayTracec() { RayTracec(this); }
   void myRayTraceFull() { RayTraceFull(this); }
-  
-  void myreadfg34_par(struct sources *src, struct apertures  *apr, struct control_flags *ifl, struct integration *xi, double *epsilon) { readfg34_par(src,apr,ifl,xi,epsilon); }
-
+  void myreadfg34_par(struct sources *src, struct apertures  *apr, struct control_flags *ifl, 
+		      struct integration *xi, double *epsilon) { readfg34_par(src,apr,ifl,xi,epsilon); }
   void myWriteBLFile(char *name) { WriteBLFile(name, this); }
   void mywritemapc(char *fname, char *header, int iord, 
-               double *ypc1, double *zpc1, double *dypc,   double *dzpc,
-	       double *wc,   double *xlc,  double *xlen1c, double *xlen2c) { writemapc(fname, header, iord, 
-               ypc1, zpc1, dypc,   dzpc,
-	       wc,   xlc,  xlen1c, xlen2c); }
-
+		   double *ypc1, double *zpc1, double *dypc,   double *dzpc,
+		   double *wc,   double *xlc,  double *xlen1c, double *xlen2c) { 
+                   writemapc(fname, header, iord, 
+			     ypc1, zpc1, dypc,   dzpc,
+			     wc,   xlc,  xlen1c, xlen2c);
   void myWriteRayFile(char *name, int *zahl, struct RayType *Rp) { WriteRayFile(name, zahl, Rp); }
   void sourceSetDefaults();
   void writeBackupFile();
