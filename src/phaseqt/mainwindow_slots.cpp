61228bc0af72d8fd1aa6ffd3df2105eb77fd7e90
flechsig <>
2012-04-18 14:47:35 +0000

    working

--- src/phaseqt/mainwindow_slots.cpp
+++ src/phaseqt/mainwindow_slots.cpp
@@ -1,6 +1,6 @@
 //  File      : /afs/psi.ch/user/f/flechsig/phase/src/phaseqt/mainwindow_slots.cpp
 //  Date      : <09 Sep 11 15:22:29 flechsig> 
-//  Time-stamp: <04 Apr 12 16:27:13 flechsig> 
+//  Time-stamp: <18 Apr 12 16:33:22 flechsig> 
 //  Author    : Uwe Flechsig, uwe.flechsig&#64;psi.&#99;&#104;
 
 //  $Source$ 
@@ -40,7 +40,8 @@ void MainWindow::about()
                "<center><a href=mailto:Johannes.Bahrdt@helmholtz-berlin.de>Johannes Bahrdt</a>, <a href=mailto:uwe.flechsig&#64;psi.&#99;&#104;>Uwe Flechsig</a> and others. </center><hr>"
                "<center>phaseqt version: '%1',<br>"
                "configured: '%2',<br>"
-               "SEVEN_ORDER: '%3'</center>").arg(VERSION).arg(CONFIGURED).arg(onoff));
+               "SEVEN_ORDER: '%3',<br>"
+	       "GRIDSIZE: '%4'</center>").arg(VERSION).arg(CONFIGURED).arg(onoff).arg(GRIDSIZE));
 } // about
 
 // UF slot
