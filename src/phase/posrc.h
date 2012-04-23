e8acf3494ee71f3e39dcb98111ffe31701951b17
flechsig <>
2012-04-23 15:58:41 +0000

    new

---
+++ src/phase/posrc.h
@@ -0,0 +1,20 @@
+/*  File      : /afs/psi.ch/user/f/flechsig/phase/src/phase/posrc.h */
+/*  Date      : <23 Apr 12 10:44:55 flechsig>  */
+/*  Time-stamp: <23 Apr 12 17:51:49 flechsig>  */
+/*  Author    : Uwe Flechsig, uwe.flechsig&#64;psi.&#99;&#104; */
+
+/*  $Source$  */
+/*  $Date$ */
+/*  $Revision$  */
+/*  $Author$  */
+
+/* soure routines for physical optics */
+
+#ifndef POSRC_H
+#define POSRC_H
+
+void source4c_ini(struct BeamlineType *);
+void source4c(struct BeamlineType *);
+
+#endif 
+/* end POSRC_H */
