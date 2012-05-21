4be661322de1fe747163f81f5f20238a732ec169
flechsig <>
2012-05-21 13:47:50 +0000

    *** empty log message ***

--- src/phaseqt/mainwindow_slots.cpp
+++ src/phaseqt/mainwindow_slots.cpp
@@ -1,6 +1,6 @@
 //  File      : /afs/psi.ch/user/f/flechsig/phase/src/phaseqt/mainwindow_slots.cpp
 //  Date      : <09 Sep 11 15:22:29 flechsig> 
-//  Time-stamp: <21 May 12 12:57:34 flechsig> 
+//  Time-stamp: <21 May 12 15:42:17 flechsig> 
 //  Author    : Uwe Flechsig, uwe.flechsig&#64;psi.&#99;&#104;
 
 //  $Source$ 
@@ -203,6 +203,7 @@ void MainWindow::activateProc(const QString &action)
       statusBar()->showMessage(tr("full ray trace-> done!"), 4000);
 
     }
+
   if (!action.compare("footprintAct")) 
     { 
       printf("\nfootprintAct button  pressed\n");
