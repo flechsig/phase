8f081080b1be9de8b930e555be726ed6659b7ec9
flechsig <>
2012-09-21 04:19:02 +0000

    clean up

--- src/phasesrv/phasesrv.c
+++ src/phasesrv/phasesrv.c
@@ -1,6 +1,6 @@
 /*  File      : /afs/psi.ch/user/f/flechsig/phase/src/phasesrv/phasesrv.c */
 /*  Date      : <14 Sep 12 16:34:45 flechsig>  */
-/*  Time-stamp: <2012-09-19 14:30:29 flechsig>  */
+/*  Time-stamp: <2012-09-21 06:18:48 flechsig>  */
 /*  Author    : Uwe Flechsig, uwe.flechsig&#64;psi.&#99;&#104; */
 
 /*  $Source$  */
@@ -26,7 +26,7 @@ int main(unsigned int argc, char *argv[])
   int setupswitch, cmode, selected, iord;
   struct BeamlineType Beamline;
   
-  Beamline.localalloc= DOALLOC;
+  Beamline.localalloc= DOALLOC;  /* phasesrv should reserve the memory */
   
   printf("phasesrv start\n");
   
@@ -34,24 +34,22 @@ int main(unsigned int argc, char *argv[])
   
   switch (setupswitch)
     {
+    case 1:  //ohne -b
     case 3:
+    case 5:  // option -b braucht ist optional
     case 7:
+    case 13: // ohne -b
     case 15:
-      printf("main: Batchmode  called\n");
+      printf("main: Batchmode  called (switch= %d)\n", setupswitch);
       BatchMode(&Beamline, cmode, selected, iord);
       exit(3);
-      break;
-    case 5:          // only filename given
-      printf("main: filename provided- do not read %s\n", (char*) MainPickName); 
-      printf("not implemented so far - usage: phasesrv -h\nexit()\n");
-      exit(3);
-      //myphaseQt.initSet(myphaseQt.myBeamline()->filenames.beamlinename);
       break;
     default:
       printf("\nusage: phasesrv -h\n\n");
       //myphaseQt.myGetPHASE((char*) MainPickName);
     }
 
-  printf("phasesrv done\n");
+  printf("phasesrv done (switch= %d)\n", setupswitch);
+  exit(0);
 }
 /* end */
