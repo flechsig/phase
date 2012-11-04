43f37c27baf6253122893e48c3a6554f05f6622e
flechsig <>
2012-11-04 11:57:46 +0000

    *** empty log message ***

--- src/opti/optisub.h
+++ src/opti/optisub.h
@@ -1,6 +1,6 @@
-/*  File      : /afs/psi.ch/user/f/flechsig/phase/src/opti/phaseopti.h */
+/*  File      : /afs/psi.ch/user/f/flechsig/phase/src/opti/optisub.h */
 /*  Date      : <04 Jan 08 14:04:24 flechsig>  */
-/*  Time-stamp: <2012-11-04 12:57:11 flechsig>  */
+/*  Time-stamp: <2012-11-04 12:57:38 flechsig>  */
 /*  Author    : Uwe Flechsig, uwe.flechsig&#64;psi.&#99;&#104; */
 
 /*  $Source$  */
