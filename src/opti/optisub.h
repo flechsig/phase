48ba00f048c9805d712ff67a953c163d2cadbe63
flechsig <>
2012-11-04 11:57:23 +0000

    *** empty log message ***

--- src/opti/optisub.h
+++ src/opti/optisub.h
@@ -1,6 +1,6 @@
 /*  File      : /afs/psi.ch/user/f/flechsig/phase/src/opti/phaseopti.h */
 /*  Date      : <04 Jan 08 14:04:24 flechsig>  */
-/*  Time-stamp: <2012-11-04 12:56:04 flechsig>  */
+/*  Time-stamp: <2012-11-04 12:57:11 flechsig>  */
 /*  Author    : Uwe Flechsig, uwe.flechsig&#64;psi.&#99;&#104; */
 
 /*  $Source$  */
@@ -20,7 +20,7 @@ double out_struct(struct BeamlineType  *, double *,  int);
 
 /* void */
 void buildsystem(struct BeamlineType *);
-void FocusSize (double *, struct BeamlineType *, double *, double *); 
+void FocusSize(double *, struct BeamlineType *, double *, double *); 
 void FullRTOpti(double *, struct BeamlineType *);
 void Get_dydz_fromSource(struct BeamlineType *, double *, double *);
 void GetFWHM(struct BeamlineType *, char *, double *);
@@ -28,7 +28,7 @@ void GetResults(struct BeamlineType *, double *, double *, double *,
 		double *, double *, double *);
 void GetRMS(struct BeamlineType *, char *, double *);
 void in_struct(struct BeamlineType *, double *, int);
-void RTOpti    (double *, struct BeamlineType *, char *);
+void RTOpti(double *, struct BeamlineType *, char *);
 void SaveOptimizedBeamline(struct BeamlineType *, struct optistruct *);
 
 #endif       
