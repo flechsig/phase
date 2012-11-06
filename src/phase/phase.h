e0e7168b153ee537d29da59fefea31aa2106f5ad
flechsig <>
2012-11-06 16:57:26 +0000

    *** empty log message ***

--- src/phase/phase.h
+++ src/phase/phase.h
@@ -1,6 +1,6 @@
 /*   File      : /afs/psi.ch/user/f/flechsig/phase/src/phase/phase.h */
 /*   Date      : <08 Mar 04 13:35:03 flechsig>  */
-/*   Time-stamp: <06 Nov 12 17:56:13 flechsig>  */
+/*   Time-stamp: <06 Nov 12 17:57:21 flechsig>  */
 /*   Author    : Uwe Flechsig, flechsig@psi.ch */
 
 /*   $Source$  */
@@ -272,10 +272,7 @@ struct xlenmaptype {
 #endif
 };
 
-  
-
 struct RayType { double y, z, dy, dz, phi; };  
-
 struct UndulatorSourceType { double length, lambda, sigvert, sighor, deltaz;};  
 struct UndulatorSource0Type { double length, lambda, sigvert, sighor, deltaz,
    sigmaez, sigmaey, sigmaedz, sigmaedy; };  
