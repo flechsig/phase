20739d991ea3c16a59ebd26e5221253d67661086
flechsig <>
2013-03-15 11:09:09 +0000

    *** empty log message ***

--- src/phase/posrc.h
+++ src/phase/posrc.h
@@ -1,6 +1,6 @@
 /*  File      : /afs/psi.ch/user/f/flechsig/phase/src/phase/posrc.h */
 /*  Date      : <23 Apr 12 10:44:55 flechsig>  */
-/*  Time-stamp: <14 Mar 13 12:29:18 flechsig>  */
+/*  Time-stamp: <15 Mar 13 12:08:34 flechsig>  */
 /*  Author    : Uwe Flechsig, uwe.flechsig&#64;psi.&#99;&#104; */
 
 /*  $Source$  */
@@ -21,7 +21,7 @@
 void check_file_consistency(struct BeamlineType *, int, int);
 void posrc_fill4(struct BeamlineType *, double *, FILE *,   int);
 void posrc_fill7(struct BeamlineType *, double *, double *, int, int, int);
-void posrc_fill8(struct BeamlineType *, double *, double *, int);
+void posrc_fill8(struct BeamlineType *, double *, double *, int, double);
 void posrc_fill_min_max(struct BeamlineType *);
 FILE *posrc_fopen(char *);
 void posrc_ini(struct BeamlineType *);
@@ -32,11 +32,11 @@ void source8c_ini(struct BeamlineType *);
 void source4c_inter_2d_(struct source_results *, double *, double *, int *);
 
 #ifdef HAVE_HDF5
-int check_hdf5_type(char *, int, int);
-void readDataDouble(hid_t, char *, double *, int);
-void readDataInt(hid_t, char *, int *, int);
-int  getDatasetSize(hid_t, char *);
-int  hasDataset(hid_t, char *);
+int   check_hdf5_type(char *, int, int);
+void  readDataDouble(hid_t, char *, double *, int);
+void  readDataInt(hid_t, char *, int *, int);
+int   getDatasetSize(hid_t, char *);
+int   hasDataset(hid_t, char *);
 hid_t myH5Fopen(char *);
 #endif
 
