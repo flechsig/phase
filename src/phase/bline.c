d000132d096d909a793a588cc6510ade0f47af6c
flechsig <>
2013-07-02 06:50:35 +0000

    *** empty log message ***

--- src/phase/bline.c
+++ src/phase/bline.c
@@ -1,6 +1,6 @@
 /*   File      : S_UF/afs/psi.ch/user/f/flechsig/phase/src/phase/bline.c */
 /*   Date      : <10 Feb 04 16:34:18 flechsig>  */
-/*   Time-stamp: <02 Jul 13 08:48:32 flechsig>  */
+/*   Time-stamp: <02 Jul 13 08:50:08 flechsig>  */
 /*   Author    : Uwe Flechsig, flechsig@psi.ch */
  
 /*   $Source$  */
@@ -478,7 +478,7 @@ void BuildBeamlineM(double lambda_local, struct BeamlineType *bl)
         
             /*printf("c: call MakemapandMatrix\n");*/  
                --elcounter;    
-	       MakeMapandMatrix(listpt, bl, &elcounter); 
+	       MakeMapandMatrix(listpt, bl, (int *)&elcounter); 
 	       ++elcounter;
 	    /* listpt-> wc,xlc,matrix,MtoSource,xlm sind erzeugt */
 	    /* wc,xlc,xlm sind richtungsabhaengig !!*/
