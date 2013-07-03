436637a9195fd9ae82a82cd62a43fa886fa63a0e
flechsig <>
2013-07-03 11:55:43 +0000

    *** empty log message ***

---
+++ src/phasempi/phasempi.c
@@ -0,0 +1,59 @@
+/*  File      : /afs/psi.ch/user/f/flechsig/phase/src/phasesrv/phasesrv.c */
+/*  Date      : <14 Sep 12 16:34:45 flechsig>  */
+/*  Time-stamp: <03 Jul 13 13:55:39 flechsig>  */
+/*  Author    : Uwe Flechsig, uwe.flechsig&#64;psi.&#99;&#104; */
+
+/*  $Source$  */
+/*  $Date$ */
+/*  $Revision$  */
+/*  $Author$  */
+
+#ifdef HAVE_CONFIG_H
+  #include "config.h"
+#endif 
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include "cutils.h" 
+#include "phase_struct.h"
+#include "phase.h"
+#include "rtrace.h"
+#include "phasesrv.h"
+
+int main(int argc, char *argv[])
+{
+  int setupswitch, cmode, selected, iord, numthreads, format;
+  struct BeamlineType Beamline;
+  
+  Beamline.localalloc= DOALLOC;  /* phasesrv should reserve the memory */ 
+  
+  printf("phasempi start\n");
+  
+  setupswitch= ProcComandLine(&Beamline.filenames, argc, argv, &cmode, 
+			      &selected, &iord, &numthreads, &format);
+
+#ifdef DEBUG 
+  strncpy(Beamline.filenames.beamlinename, "test_5000.phase", MaxPathLength- 1);  /* for debugging */
+#endif 
+
+  switch (setupswitch)
+    {
+    case 1:  // ohne -b
+    case 3:
+    case 5:  // option -b braucht ist optional
+    case 7:
+    case 13: // ohne -b
+    case 15:
+      printf("main: Batchmode  called (switch= %d)\n", setupswitch);
+      BatchMode(&Beamline, cmode, selected, iord, numthreads, format);
+      break;
+    default:
+      printf("\nusage: phasesrv -h\n\n");
+    }
+
+  printf("phasempi done (switch= %d)\n", setupswitch);
+  exit(0);
+}
+/* end */
---
+++ src/phasempi/phasempi.h
@@ -0,0 +1,16 @@
+/*  File      : /afs/psi.ch/user/f/flechsig/phase/src/phasesrv/phasesrv.h */
+/*  Date      : <14 Sep 12 16:33:56 flechsig>  */
+/*  Time-stamp: <03 Jul 13 13:55:04 flechsig>  */
+/*  Author    : Uwe Flechsig, uwe.flechsig&#64;psi.&#99;&#104; */
+
+/*  $Source$  */
+/*  $Date$ */
+/*  $Revision$  */
+/*  $Author$  */
+
+#ifdef PHASEMPI_H
+
+
+
+#endif
+/* end */
