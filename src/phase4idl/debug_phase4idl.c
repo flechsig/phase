6fefef45dc72b19050446f3852caad789150c4ef
flechsig <>
2013-08-16 15:00:18 +0000

    add a test script to check for segmentation fault

---
+++ src/phase4idl/debug_phase4idl.c
@@ -0,0 +1,20 @@
+/*  File      : /afs/psi.ch/user/f/flechsig/phase/src/phase4idl/debug_phase4idl.c */
+/*  Date      : <16 Aug 13 16:40:16 flechsig>  */
+/*  Time-stamp: <16 Aug 13 16:59:54 flechsig>  */
+/*  Author    : Uwe Flechsig, uwe.flechsig&#64;psi.&#99;&#104; */
+
+/*  $Source$  */
+/*  $Date$ */
+/*  $Revision$  */
+/*  $Author$  */
+
+#include <stdio.h>
+#include "source4x.h"
+
+main ()
+{
+  struct source4idl  s4;
+  struct source4cidl s4c;
+
+  printf("debug_phase4idl.c called\n");
+}
