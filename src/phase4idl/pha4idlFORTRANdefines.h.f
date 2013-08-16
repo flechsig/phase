e4be61620ac458c87f6e054a581a5c410a1b6ebc
flechsig <>
2013-08-16 07:39:00 +0000

    add comment- no function change

--- src/phase4idl/pha4idlFORTRANdefines.h.f
+++ src/phase4idl/pha4idlFORTRANdefines.h.f
@@ -1,8 +1,20 @@
+c$$$ File      : /afs/psi.ch/user/f/flechsig/phase/src/phase4idl/pha4idlFORTRANdefines.h.f
+c$$$ Date      : <16 Aug 13 09:36:19 flechsig> 
+c$$$ Time-stamp: <16 Aug 13 09:38:46 flechsig> 
+c$$$ Author    : Uwe Flechsig, uwe.flechsig&#64;psi.&#99;&#104;
+c$$$
+c$$$ $Source$ 
+c$$$ $Date$
+c$$$ $Revision$ 
+c$$$ $Author$ 
 
 cc /// pha4idlFORTRANdefines.h.f
 
+c UF: by including this file the fortran routines with _nostructs become independent from the c- code- the field dimensions are statically defined and not via configure
 
 cc MaxDim is the Dimension of the fieldsw in the src4-structure
 #define MaxDim 2048
 cc BigMaxDim is for internal temporary fields
 #define BigMaxDim 2048 
+
+c end
