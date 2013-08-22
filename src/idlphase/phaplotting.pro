641fc8b57a0fba9951f92ad3ee3c6abb92c578b6
flechsig <>
2013-08-22 13:53:48 +0000

    does not work

--- src/idlphase/phaplotting.pro
+++ src/idlphase/phaplotting.pro
@@ -1,6 +1,6 @@
 ;  File      : /afs/psi.ch/user/f/flechsig/phase/src/idlphase/phaplotting.pro
 ;  Date      : <20 Aug 13 08:40:08 flechsig> 
-;  Time-stamp: <22 Aug 13 14:13:45 flechsig> 
+;  Time-stamp: <22 Aug 13 14:40:03 flechsig> 
 ;  Author    : Uwe Flechsig, uwe.flechsig&#64;psi.&#99;&#104;
 
 ;  $Source$ 
@@ -436,6 +436,7 @@ z=get_pha_src4_axis_z(beam)
 if n_elements(zpol) eq 0 then f= complex(beam.zeyre, beam.zeyim) else f= complex(beam.zezre, beam.zezim)
 
 phase= atan(f, /phase)
+help,phase
 
 mycontour, (phase(0:beam.iezrex-1,0:beam.iezrey-1)), z, y, title=name
 
--- src/phase4idl/pha4idlFORTRANdefines.h.f
+++ src/phase4idl/pha4idlFORTRANdefines.h.f
@@ -1,6 +1,6 @@
 c$$$ File      : /afs/psi.ch/user/f/flechsig/phase/src/phase4idl/pha4idlFORTRANdefines.h.f
 c$$$ Date      : <16 Aug 13 09:36:19 flechsig> 
-c$$$ Time-stamp: <20 Aug 13 08:29:01 flechsig> 
+c$$$ Time-stamp: <22 Aug 13 14:30:25 flechsig> 
 c$$$ Author    : Uwe Flechsig, uwe.flechsig&#64;psi.&#99;&#104;
 c$$$
 c$$$ $Source$ 
@@ -15,6 +15,10 @@ c UF: by including this file the fortran routines with _nostructs become indepen
 cc MaxDim is the Dimension of the fieldsw in the src4-structure
 #define MaxDim 2048
 cc BigMaxDim is for internal temporary fields
-#define BigMaxDim 2048 
-
+#define BigMaxDim  2048
+#define BIGMAXDIM2 4096
+cc 6 * MaxDim = 12288
+#define STACK4SPLINE 12288
+cc 2*MaxDim*MaxDim+16 = 8388624
+#define STACK4FFT 8388624
 c end
