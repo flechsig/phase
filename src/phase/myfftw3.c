d482cb71827afdc3f236df8938cb238d9a070cd6
flechsig <>
2014-01-06 13:17:03 +0000

    new

---
+++ src/phase/myfftw3.c
@@ -0,0 +1,13 @@
+ /* File      : /afs/psi.ch/user/f/flechsig/phase/src/phase/myfftw3.c */
+ /* Date      : <06 Jan 14 14:13:01 flechsig>  */
+ /* Time-stamp: <06 Jan 14 14:13:20 flechsig>  */
+ /* Author    : Uwe Flechsig, uwe.flechsig&#64;psi.&#99;&#104; */
+
+ /* $Source$  */
+ /* $Date$ */
+ /* $Revision$  */
+ /* $Author$  */
+
+
+
+/* end */
