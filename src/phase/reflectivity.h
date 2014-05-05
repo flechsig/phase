e21a9e43389f16cf7f02d03752cd82c82d792d8a
flechsig <>
2014-05-05 14:44:54 +0000

    *** empty log message ***

---
+++ src/phase/reflectivity.h
@@ -0,0 +1,19 @@
+ /* File      : /afs/psi.ch/project/phase/src/phase/reflectivity.h */
+ /* Date      : <05 May 14 16:42:48 flechsig>  */
+ /* Time-stamp: <05 May 14 16:43:55 flechsig>  */
+ /* Author    : Uwe Flechsig, uwe.flechsig&#64;psi.&#99;&#104; */
+
+ /* $Source$  */
+ /* $Date$ */
+ /* $Revision$  */
+ /* $Author$  */
+ 
+
+#ifndef REFLECTIVITY_H 
+#define REFLECTIVITY_H 1
+
+// protoptype in alphabetic order
+void apply_reflectivity_(int *, double *, double *, double *, double *, double *, double *);
+
+#endif
+// end /afs/psi.ch/project/phase/src/phase/reflectivity.h
