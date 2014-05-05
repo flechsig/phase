1e939dc0ab451b0e66d9e99b84ea04173adea00b
flechsig <>
2014-05-05 12:51:25 +0000

    add height error framework

--- src/phase/heighterror.c
+++ src/phase/heighterror.c
@@ -1,12 +1,13 @@
  /* File      : /afs/psi.ch/project/phase/src/phase/heighterror.c */
  /* Date      : <05 May 14 14:12:11 flechsig>  */
- /* Time-stamp: <05 May 14 14:26:52 flechsig>  */
+ /* Time-stamp: <05 May 14 14:47:17 flechsig>  */
  /* Author    : Uwe Flechsig, uwe.flechsig&#64;psi.&#99;&#104; */
 
  /* $Source$  */
  /* $Date$ */
  /* $Revision$  */
  /* $Author$  */
+
 
 #ifdef HAVE_CONFIG_H
   #include "config.h"
@@ -25,14 +26,14 @@
 #include "heighterror.h"
 
 
-void ApplyHeightError_(int *blp, double *eyre, double *eyim, double *ezre, double *ezim, double *z, double *y, double *dz, double *dy)
+void apply_height_error_(int *blp, double *eyre, double *eyim, double *ezre, double *ezim, double *z, double *y, double *dz, double *dy)
 {
   struct BeamlineType *bl;
 
   bl = (struct BeamlineType *)blp;
 
 #ifdef DEBUG
-  printf("debug: %s, ApplyHeightError_ called\n", __FILE__);
+  printf("\ndebug: %s, ApplyHeightError_ called\n", __FILE__);
 #endif
     /// some code
 
@@ -40,6 +41,6 @@ void ApplyHeightError_(int *blp, double *eyre, double *eyim, double *ezre, doubl
   printf("debug: %s, ApplyHeightError_ end\n", __FILE__);
 #endif
 
-} // end ApplyHeightError
+} // end apply_height_error_
 
 // end /afs/psi.ch/project/phase/src/phase/heighterror.c
--- src/phase/heighterror.h
+++ src/phase/heighterror.h
@@ -1,6 +1,6 @@
  /* File      : /afs/psi.ch/project/phase/src/phase/heighterror.h */
  /* Date      : <05 May 14 14:17:45 flechsig>  */
- /* Time-stamp: <05 May 14 14:19:16 flechsig>  */
+ /* Time-stamp: <05 May 14 14:46:12 flechsig>  */
  /* Author    : Uwe Flechsig, uwe.flechsig&#64;psi.&#99;&#104; */
 
  /* $Source$  */
@@ -9,11 +9,12 @@
  /* $Author$  */
 
 #ifndef HEIGHTERROR_H 
-#define HEIGHTERROR_H
+#define HEIGHTERROR_H 1
 
+#define WITH_HEIGHT_ERRORS 1
 
 // protoptype in alphabetic order
-void ApplyHeightError_(int *, double *, double *, double *, double *, double *, double *, double *, double *);
+void apply_height_error_(int *, double *, double *, double *, double *, double *, double *, double *, double *);
 
 #endif
 // end /afs/psi.ch/project/phase/src/phase/heighterror.h
