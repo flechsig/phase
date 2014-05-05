37a555dbe5e8c6e1dcdb44cf908ad0b83722f225
flechsig <>
2014-05-05 12:22:05 +0000

    new file

---
+++ src/phase/heighterror.c
@@ -0,0 +1,45 @@
+ /* File      : /afs/psi.ch/project/phase/src/phase/heighterror.c */
+ /* Date      : <05 May 14 14:12:11 flechsig>  */
+ /* Time-stamp: <05 May 14 14:20:45 flechsig>  */
+ /* Author    : Uwe Flechsig, uwe.flechsig&#64;psi.&#99;&#104; */
+
+ /* $Source$  */
+ /* $Date$ */
+ /* $Revision$  */
+ /* $Author$  */
+
+#ifdef HAVE_CONFIG_H
+  #include "config.h"
+#endif 
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+#include "cutils.h"
+#include "phase_struct.h"
+#include "phase.h"
+#include "posrc.h"
+#include "rtrace.h"
+#include "common.h"
+#include "heighterror.h"
+
+
+void ApplyHeightError_(int *blp, double *eyre, double *eyim, double *ezre, double *ezim, double *z, double *y, double *dz, double *dy)
+{
+  struct BeamlineType *bl;
+
+  bl = (struct BeamlineType *)blp;
+
+#ifdef DEBUG
+  printf("debug: %s, ApplyHeightError_ called\n", __FILE__)
+#endif
+    /// some code
+
+#ifdef DEBUG
+  printf("debug: %s, ApplyHeightError_ end\n", __FILE__)
+#endif
+
+} // end ApplyHeightError
+
+// end /afs/psi.ch/project/phase/src/phase/heighterror.c
---
+++ src/phase/heighterror.h
@@ -0,0 +1,19 @@
+ /* File      : /afs/psi.ch/project/phase/src/phase/heighterror.h */
+ /* Date      : <05 May 14 14:17:45 flechsig>  */
+ /* Time-stamp: <05 May 14 14:19:16 flechsig>  */
+ /* Author    : Uwe Flechsig, uwe.flechsig&#64;psi.&#99;&#104; */
+
+ /* $Source$  */
+ /* $Date$ */
+ /* $Revision$  */
+ /* $Author$  */
+
+#ifndef HEIGHTERROR_H 
+#define HEIGHTERROR_H
+
+
+// protoptype in alphabetic order
+void ApplyHeightError_(int *, double *, double *, double *, double *, double *, double *, double *, double *);
+
+#endif
+// end /afs/psi.ch/project/phase/src/phase/heighterror.h
