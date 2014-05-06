47a29b5fceee05112ecb7ba75bb2f37f26696f8b
flechsig <>
2014-05-06 14:28:42 +0000

    worked on reflectivity- not finished

--- src/phase/reflectivity.c
+++ src/phase/reflectivity.c
@@ -1,6 +1,6 @@
 /* File      : /afs/psi.ch/project/phase/src/phase/reflectivity.c */
 /* Date      : <05 May 14 16:40:19 flechsig>  */
-/* Time-stamp: <05 May 14 17:14:10 flechsig>  */
+/* Time-stamp: <06 May 14 16:27:40 flechsig>  */
 /* Author    : Uwe Flechsig, uwe.flechsig&#64;psi.&#99;&#104; */
 
 /* $Source$  */
@@ -29,6 +29,9 @@ void apply_reflectivity_(int *blp, double *eyre, double *eyim, double *ezre, dou
 {
   struct BeamlineType *bl;
   double yamp, ypha, zamp, zpha;
+  double f1, f2, f1tab, f2tab, entab, a, rho;
+  int    z;
+  char   *element= "Au";
 
   bl = (struct BeamlineType *)blp;
 
@@ -40,6 +43,9 @@ void apply_reflectivity_(int *blp, double *eyre, double *eyim, double *ezre, dou
   zamp= sqrt(pow(*ezre, 2.0) + pow(*ezim, 2.0));
   ypha= atan2(*eyim, *eyre);
   zpha= atan2(*ezim, *ezre);
+
+  //  ReadHenke(element, &entab, &f1tab, &f2tab);
+  // ReadMaterial(element, &z, &a, &rho);
 
   /*
   *eyre= yamp* cos(ypha);
@@ -53,4 +59,68 @@ void apply_reflectivity_(int *blp, double *eyre, double *eyim, double *ezre, dou
 #endif
 } // end apply_reflectivity
 
+void ReadHenke(char *element, double *en0, double *f10, double *f20)
+{
+  char tabname[MaxPathLength];
+  FILE *f;
+
+#ifdef DEBUG
+  printf("debug: ReadHenke called for %s\n", element);
+#endif
+
+  snprintf(tabname, (MaxPathLength- 1), "%s/share/phase/%s.f12\0", getenv("PHASE_HOME"), element);
+  if ((f= fopen(tabname, "r")) == NULL) 
+    {
+      fprintf(stderr, "error can't find Henke Table %s - return\n", tabname);
+      return;
+    }  
+
+  fclose(f);
+} // ReadHenke
+
+void ReadMaterial(char *element, int *z, double *a, double *rho)
+{
+  char tabname[MaxPathLength], buffer[MaxPathLength], str[10];
+  FILE *f;
+  int  found;
+
+#ifdef DEBUG
+  printf("debug: ReadMaterial called for %s\n", element);
+#endif
+  *a= *rho= *z= -1;
+  snprintf(tabname, (MaxPathLength- 1), "%s/share/phase/rhoatom.dat\0", getenv("PHASE_HOME"));
+  if ((f= fopen(tabname, "r")) == NULL) 
+    {
+      fprintf(stderr, "error can't find Material Table %s - return\n", tabname);
+      return;
+    }
+
+  fgets(buffer, (MaxPathLength-1), f);   // read first line
+  found= strncmp(buffer, "Element\tZ\tA\t\trho", 16); // check 1st line
+  if (found != 0)
+    {
+      fprintf(stderr, "error: Material Table %s has not the expected format- return\n", tabname);
+      return;
+    }
+
+  while (! feof(f) && ! found)
+    {
+      fgets(buffer, (MaxPathLength-1), f);
+      found= (strncmp(buffer, element, 2) == 0) ? 1 : 0;
+    }
+
+  fclose(f);
+
+  if (! found)
+    {
+      fprintf(stderr, "error: Element %s not found in Material Table %s - return\n", element, tabname);
+      return;
+    }
+
+  sscanf(buffer, "%9s\t%d\t%lf\t\t%lf", str, z, a, rho);
+
+#ifdef DEBUG1
+  printf("debug: found=%sparsed: %s, %d, %lf, %lf\n", buffer, str, *z, *a, *rho);
+#endif
+} // ReadMaterial
 // end /afs/psi.ch/project/phase/src/phase/reflectivity.c
--- src/phase/reflectivity.h
+++ src/phase/reflectivity.h
@@ -1,6 +1,6 @@
  /* File      : /afs/psi.ch/project/phase/src/phase/reflectivity.h */
  /* Date      : <05 May 14 16:42:48 flechsig>  */
- /* Time-stamp: <05 May 14 16:51:27 flechsig>  */
+ /* Time-stamp: <06 May 14 16:13:46 flechsig>  */
  /* Author    : Uwe Flechsig, uwe.flechsig&#64;psi.&#99;&#104; */
 
  /* $Source$  */
@@ -12,8 +12,12 @@
 #ifndef REFLECTIVITY_H 
 #define REFLECTIVITY_H 1
 
+#define NA       6.0221e23                  // Avogadronumber
+#define RE       2.81794e-15                // Classical electron radius (m)
+
 // protoptype in alphabetic order
 void apply_reflectivity_(int *, double *, double *, double *, double *);
-
+void ReadHenke(char *, double *, double *, double *);
+void ReadMaterial(char *, int *, double *, double *);
 #endif
 // end /afs/psi.ch/project/phase/src/phase/reflectivity.h
