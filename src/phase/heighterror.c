1bb4524b4ffd307ebd49aee9cdf1a506dc299a6a
flechsig <>
2014-06-04 13:13:02 +0000

    should now work also outside

--- src/phase/heighterror.c
+++ src/phase/heighterror.c
@@ -1,6 +1,6 @@
  /* File      : /afs/psi.ch/project/phase/src/phase/heighterror.c */
  /* Date      : <05 May 14 14:12:11 flechsig>  */
- /* Time-stamp: <04 Jun 14 08:49:33 flechsig>  */
+ /* Time-stamp: <04 Jun 14 15:12:48 flechsig>  */
  /* Author    : Uwe Flechsig, uwe.flechsig&#64;psi.&#99;&#104; */
 
  /* $Source$  */
@@ -166,10 +166,7 @@ void surf_height_interp(struct SurfaceType *sf, double *wwert, double *lwert, do
   diwl= diw* dil;
 
   // calculation of weight factors depends on 1d or 2d
-  // UF Jun 14 borders and outside region to be checked if interpolation is correct  
-
-  *u_interp= u1;    // the default value for return is index 0
-
+  
   if (diwl && (fabs(dwl) > ZERO)) // 2d case inside
     {
       factor1  = (*wwert- w1)* (*lwert- l1) / dwl;
@@ -194,10 +191,8 @@ void surf_height_interp(struct SurfaceType *sf, double *wwert, double *lwert, do
 	      factor4 = (l2- *lwert) / dl;
 	      *u_interp = factor2* u3 + factor4* u1; // weighted sum 
 	    } 
-	  else // what happens outside ??
-	    {
-	      printf("warning: %s region in error file too small- to handle that case is not yet ready\n", __FILE__);
-	    }
+	  else     // outside defined region i.e. dil=diw=0
+	    *u_interp= u1;  	      
 	} // end 1d 
     } // end 
   // end of the interpolation
