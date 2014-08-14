0bd681cdada09b2b7b3a46d4dd4d641653c76915
flechsig <>
2014-08-14 06:32:34 +0000

    dd

--- src/fkoempi/fkoempi.c
+++ src/fkoempi/fkoempi.c
@@ -1,6 +1,6 @@
  /* File      : /afs/psi.ch/user/f/flechsig/phase/src/fkoempi/fkoempi.c */
  /* Date      : <01 Apr 14 09:15:01 flechsig>  */
- /* Time-stamp: <01 Apr 14 09:15:44 flechsig>  */
+ /* Time-stamp: <13 Aug 14 14:06:35 flechsig>  */
  
  /* $Source$  */
  /* $Date$ */
@@ -19,26 +19,9 @@
 #include <math.h>
 #include <stdlib.h>
 
+#include "fkoempi.h"
 
-#define MAXDIM 2048
-
-#define PARFNAME    "fkoempi.par"
-
-#define BASEOFNAME   "fout"
-
-struct extends
-{
-  double y_min;
-  double y_max;
-  double z_min;
-  double z_max;
-};
-
-struct complex
-{
-  double re, im;
-};
-
+/* global vars */
 struct complex ey0[MAXDIM][MAXDIM];
 struct complex ez0[MAXDIM][MAXDIM];
 struct complex ey1[MAXDIM][MAXDIM];
@@ -63,18 +46,6 @@ double dz0, dy0;
 struct extends e1_extends, s_extends;
 //double z1_min, z1_max, y1_min, y1_max;
 
-
-void all_load_parameters(const char *parfname);
-void all_load_fields();
-void master_save_fields(const char *baseofname, int ny, int nz);
-void slave_load_surface(const char *surffilename, int *ny, int *nz, double sy[], double sz[]);
-
-void build_dest_axis(const struct extends *e, int ny, int nz, double sy[], double sz[]);
-
-void master_propagate(int nslaves);
-void slave_propagate_1(double angle);
-void slave_propagate_2(double angle);
-void slave_propagate_10();
 
 
 /************************************************************************
