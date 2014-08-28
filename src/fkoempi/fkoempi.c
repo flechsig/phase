9063a58d23350731372dc3e6f798f997fed5bc65
flechsig <>
2014-08-28 13:38:08 +0000

    add header

--- src/fkoempi/fkoempi.c
+++ src/fkoempi/fkoempi.c
@@ -1,11 +1,39 @@
  /* File      : /afs/psi.ch/user/f/flechsig/phase/src/fkoempi/fkoempi.c */
  /* Date      : <01 Apr 14 09:15:01 flechsig>  */
- /* Time-stamp: <13 Aug 14 14:06:35 flechsig>  */
+ /* Time-stamp: <28 Aug 14 15:37:55 flechsig>  */
  
  /* $Source$  */
  /* $Date$ */
  /* $Revision$  */
  /* $Author$  */
+
+/* *****************************************************************************
+!
+!   Copyright (C) 2014 Helmholtz-Zentrum Berlin, Germany and 
+!                      Paul Scherrer Institut Villigen, Switzerland
+!   
+!   Author Johannes Bahrdt, johannes.bahrdt@helmholtz-berlin.de
+!          Uwe Flechsig,    uwe.flechsig@psi.ch
+!
+! ------------------------------------------------------------------------------
+!
+!   This file is part of PHASE.
+!
+!   PHASE is free software: you can redistribute it and/or modify
+!   it under the terms of the GNU General Public License as published by
+!   the Free Software Foundation, version 3 of the License, or
+!   (at your option) any later version.
+!
+!   PHASE is distributed in the hope that it will be useful,
+!   but WITHOUT ANY WARRANTY; without even the implied warranty of
+!   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!   GNU General Public License for more details.
+!
+!   You should have received a copy of the GNU General Public License
+!   along with PHASE (src/LICENSE).  If not, see <http://www.gnu.org/licenses/>. 
+!
+! ***************************************************************************** */
+
 
 /************************************************************************
  fkoe.c
