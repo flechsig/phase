241f39ae49be5264ac91dbc85286e9a5b028debf
Uwe Flechsig <uwe.flechsig@psi.ch>
2014-09-02 12:22:55 +0200

    add header

--- src/phasempi/phasempiexample.sh
+++ src/phasempi/phasempiexample.sh
@@ -1,13 +1,41 @@
 #!/bin/bash
  # File      : /afs/psi.ch/user/f/flechsig/phase/src/phasempi/phasempiexample.sh
  # Date      : <30 Apr 14 10:26:54 flechsig> 
- # Time-stamp: <30 Apr 14 10:27:24 flechsig> 
+ # Time-stamp: <02 Sep 14 12:21:22 flechsig> 
  # Author    : Uwe Flechsig, uwe.flechsig&#64;psi.&#99;&#104;
 
  # $Source$ 
  # $Date$
  # $Revision$ 
  # $Author$ 
+
+# ******************************************************************************
+#
+#   Copyright (C) 2014 Helmholtz-Zentrum Berlin, Germany and 
+#                      Paul Scherrer Institut Villigen, Switzerland
+#   
+#   Author Johannes Bahrdt, johannes.bahrdt@helmholtz-berlin.de
+#          Uwe Flechsig,    uwe.flechsig@psi.ch
+#
+# ------------------------------------------------------------------------------
+#
+#   This file is part of PHASE.
+#
+#   PHASE is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, version 3 of the License, or
+#   (at your option) any later version.
+#
+#   PHASE is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with PHASE (src/LICENSE).  If not, see <http:#www.gnu.org/licenses/>. 
+#
+# ******************************************************************************
+
 
 ## phasempi example script for merlin cluster at PSI
 
