dad7b19bf9edb19aa8fd93416a6fab44547c1dc9
Uwe Flechsig <uwe.flechsig@psi.ch>
2014-09-10 16:45:01 +0200

    new

---
+++ src/PhaseStation/images.sh
@@ -0,0 +1,11 @@
+# is called at the beginning
+# man kiwi::images.sh
+# to install additional packages
+test -f /.kconfig && . /.kconfig
+test -f /.profile && . /.profile
+
+echo "@@@@@@@@@@@@@@@@@@@@@@@@@@ call images.sh @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+#cp /usr/local/bin/phaseqt /scratch1/myphasestation/usr/local/bin
+
+
+exit
