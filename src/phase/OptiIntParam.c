40fa201d624ffe714b1d3f3bd1c0f729b4e6b3dc
Uwe Flechsig <uwe.flechsig@psi.ch>
2014-12-15 12:01:03 +0100

    add header

--- src/phase/OptiIntParam.c
+++ src/phase/OptiIntParam.c
@@ -1,7 +1,35 @@
-/*   $Source$  */
-/*   $Date$ */
-/*   $Revision$  */
-/*   $Author$  */
+/* File      : /afs/psi.ch/project/phase/GIT/phase/src/phase/OptiIntParam.c */
+/* Date      : <15 Dec 14 11:59:33 flechsig>  */
+/* Time-stamp: <15 Dec 14 12:00:53 flechsig>  */
+/* Author    : Uwe Flechsig, uwe.flechsig&#64;psi.&#99;&#104; */
+
+// ******************************************************************************
+//
+//   Copyright (C) 2014 Helmholtz-Zentrum Berlin, Germany and 
+//                      Paul Scherrer Institut Villigen, Switzerland
+//   
+//   Author Johannes Bahrdt, johannes.bahrdt@helmholtz-berlin.de
+//          Uwe Flechsig,    uwe.flechsig@psi.ch
+//
+// ------------------------------------------------------------------------------
+//
+//   This file is part of PHASE.
+//
+//   PHASE is free software: you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation, version 3 of the License, or
+//   (at your option) any later version.
+//
+//   PHASE is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+//
+//   You should have received a copy of the GNU General Public License
+//   along with PHASE (src/LICENSE).  If not, see <http://www.gnu.org/licenses/>. 
+//
+// ******************************************************************************
+
 
 #ifdef HAVE_CONFIG_H
   #include "config.h"
@@ -261,3 +289,5 @@ printf("Autorange final: best set found: (%.2f, %.2f, %.2f, %.2f)mrad\n", y1*1E3
   //restoring range struct
   memcpy(psip, &backup_psip, sizeof(struct PSImageType));
 }
+
+// end of file
