aebd379dd2b52bb10e4df4fd5f9432818ec9cd6e
Uwe Flechsig <uwe.flechsig@psi.ch>
2015-05-26 12:26:21 +0200

    add header

--- src/bin/myqstat.pl
+++ src/bin/myqstat.pl
@@ -1,7 +1,46 @@
 #!/usr/bin/perl -w
+# File      : /afs/psi.ch/project/phase/GIT/phase/src/bin/myqstat.pl
+# Date      : <26 May 15 12:24:12 flechsig> 
+# Time-stamp: <26 May 15 12:26:13 flechsig> 
+# Author    : Uwe Flechsig, uwe.flechsig&#64;psi.&#99;&#104;
+
+# $Source$ 
+# $Date$
+# $Revision$ 
+# $Author$ 
+
+# ******************************************************************************
+#
+#   Copyright (C) 2015 Helmholtz-Zentrum Berlin, Germany and 
+#                      Paul Scherrer Institut Villigen, Switzerland
+#   
+#   Author Johannes Bahrdt, johannes.bahrdt@helmholtz-berlin.de
+#          Uwe Flechsig,    uwe.flechsig@psi.ch
+#
+# ------------------------------------------------------------------------------
+#
+#   This file is part of PHASE.
+#
+#   PHASE is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, version 3 of the License, or
+#   (at your option) any later version.
+#
+#   PHASE is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with PHASE (src/LICENSE).  If not, see <http://www.gnu.org/licenses/>. 
+#
+# ******************************************************************************
+
+# a compressed multi user queue status for clusters 
 
 my $cmd= "qstat -u '*'";
-my ($i, $line, @lines, %idh, @arr, $jid, %lh, %sh, @sline, $i0, $j, $k, $mk, $slots, %idh1, %sh1)= (0,'',);
+my ($i, $line, @lines, %idh, @arr, $jid, %lh, %sh, @sline, $i0, $j, $k, 
+    $mk, $slots, %idh1, %sh1)= (0,'',);
 
 @lines= `$cmd`;        # store output
 $line= shift @lines;   # skip 2 header lines
