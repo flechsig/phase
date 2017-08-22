80ac6ae21b6e03501d63d069eefe9900e1d40c4a
Uwe Flechsig <uwe.flechsig@psi.ch>
2017-08-22 08:45:43 +0200

    xx

--- src/phasepython/phase.py
+++ src/phasepython/phase.py
@@ -1,6 +1,6 @@
 # File      : /afs/psi.ch/project/phase/GIT/phase/src/phasepython/phase.py
 # Date      : <15 Aug 17 16:25:49 flechsig> 
-# Time-stamp: <18 Aug 17 17:01:50 flechsig> 
+# Time-stamp: <21 Aug 17 17:12:51 flechsig> 
 # Author    : Uwe Flechsig, uwe.flechsig&#64;psi.&#99;&#104;
 
 # $Source$ 
@@ -57,6 +57,31 @@ class emf(object):
 
         return s   
 
+   def aperture(type=1):
+      """acts as an aperture, or generates wavefield with amplitude according to 'type'
+
+      Args:
+         field
+         example
+         type=1 (int):      type of aperture
+            type 1  : rectangular              P1 = hsize, P2 = vsize
+            type 2  : vertical slit            P1 = hsize, P2 = hpos (default= 0)
+            type 3  : horizontal slit          P1 = vsize, P2 = vpos (default= 0)
+            type 12 : double slit vertical,    P1 = hsize, P2= hsep
+            type 13 : double slit horizontal,  P1 = vsize, P2= vsep
+            type 20 : circular                 P1 = Radius
+                      Radius > 0: central circular part is transparent
+                      Radius < 0: central circular part is oblique
+            type 21 : annular                  P1 = Outer radius, P2 = inner radius
+            type 32 : vertical mirror          P1 = length, P2 = grazing angle (rad)
+            type 33 : horizontalal mirror      P1 = length, P2 = grazing angle (rad)
+
+      Example:
+          >>> emf = phase.initphase()
+              emf.aperture(example=True)
+      """
+   # end aperture
+
    def gaussbeam(self, dist=None, drift=None, w0=1e-5, Nz=243, Ny=None, sizez=1e-3, sizey=None, \
                wavelength=1e-10, plot=False, example=False, \
                z_off=0.0, y_off=0.0):
@@ -87,7 +112,6 @@ class emf(object):
            print('example: HeNe Laser ')
            print('wavelength=633e-9, w0= 1e-3, dist= 10., sizez=1e-2')
            print('**********************************************************')
-           #self.gaussbeam(dist=10., wavelength=633e-9, w0=1e-3, sizez=1e-2, plot=True)
            self.gaussbeam(dist=10., wavelength=633e-9, w0=1e-3, sizez=1e-2, plot=True)
            print('**********************************************************')
            print('end example')
@@ -142,7 +166,7 @@ class emf(object):
        # norm to 0.5 W  !! we assume only one polarization 
        intensity = np.absolute(field)**2/377.0
        binsize= (z_vec[1]-z_vec[0])*(y_vec[1]-y_vec[0])
-       itot= np.sum(intensity)*binsize*2.0
+       itot= np.sum(intensity) * binsize * 2.0
        scale= 1.0/np.sqrt(itot)
        field*= scale
 
@@ -150,7 +174,6 @@ class emf(object):
            print ('!! warning -- some outside points are truncated !!')
 
 # plot using mycontour
-#       plot= False
        if plot is True:
            print('plot')
            bamp = np.absolute(field)
@@ -203,6 +226,34 @@ class emf(object):
                   fontsize=10)
        plt.show()
        # mycontour
+
+   def plotamplitude( self ):
+      """plot the amplitude"""
+      
+      title= self.getname()+ " amplitude"
+      self.mycontour(self.getamplitude(), self.getz_vec() * 1e3, self.gety_vec() * 1e3, zlabel= 'amplitude ($V/m^3$)', title=title)
+   # end plotamplitude 
+ 
+   def plotimag( self ):
+      """plot the imaginary part of the field"""
+      
+      title= self.getname()+ " imaginary part"
+      self.mycontour(self.getimag(), self.getz_vec() * 1e3, self.gety_vec() * 1e3, zlabel= 'imaginary part ()', title=title)
+   # end plotimag
+
+   def plotphotons( self ):
+      """plot the field intensity as photons"""
+      
+      title= self.getname()+ " photons"
+      self.mycontour(self.getphotons(), self.getz_vec() * 1e3, self.gety_vec() * 1e3, zlabel= 'photon density ($1/m^2$)', title=title)
+   # end plotphotons
+
+   def plotreal( self ):
+      """plot the real partof the field"""
+      
+      title= self.getname()+ " real part"
+      self.mycontour(self.getreal(), self.getz_vec() * 1e3, self.gety_vec() * 1e3, zlabel= 'real part ()', title=title)
+   # end plotreal  
    
    def propfraunhofer(self, drift=None):
        '''propfraunhofer'''
@@ -259,7 +310,10 @@ class emf(object):
    # end propfraunhofer
 
    def propfresnel(self, drift=None):
-       '''propfresnel'''
+       """propfresnel
+
+       Args:
+       """
        field = self.field
        y_vec = self.y_vec
        z_vec = self.z_vec
@@ -314,7 +368,7 @@ class emf(object):
        scale   = np.zeros((nz, ny), dtype=complex)                                     # make a complex array
        for i in range(0, nz-1):
            for j in range(0, ny-1):
-               phase = (u[i]**2 + v[j]**2) * k/(2.0*drift)             
+               phase = (u[i]**2 + v[j]**2) * k/(2.0 * drift)             
                phase = phase + (u[i]*z0 + v[j] * y0) * k / drift        # set origin  12.9.2013: changed sign from - to +
                scale[i,j]= complex(np.cos(phase), np.sin(phase))   
        
@@ -330,24 +384,64 @@ class emf(object):
 
        #propfresnel
 
+   def getamplitude( self ):
+       """returns the amplitude of the field
+
+       Returns:
+          field (real): amplitude as numpy array
+       """
+       return np.abs(self.field)
+   # getamplitude
+
+
    def getfield( self ):
-       "returns the field"
+       """returns the field
+
+       Returns:
+          field (complex): field as numpy array
+       """
        return self.field
+   # getfield
 
    def getimag( self ):
-       "returns the imaginary part of the field"
-       return imag(self.field)
+       """returns the imaginary part of the field
+
+       Returns:
+          field (double): imaginary part as numpy array
+       """
+       return np.imag(self.field)
+   # getimag
 
    def getintensity( self ):
-       "returns the intensity"
+       """returns the intensity
+
+       Returns:
+          field (double): intensity in W as numpy array
+       """
        return np.absolute(self.field)**2/377.0
+   # getintensity
 
    def getname( self ):
-       "returns the nameof the field"
+       """returns the nameof the field
+
+       Returns:
+          name (string): the name
+       """
        return self.name
+   # getname
 
    def getphase(self, param='raw'):
-       "returns the phase of the field"
+       """returns the phase of the field
+
+       Args:
+          param='raw' (string): the phase style ("raw", "herra", "numpy")
+             "raw": no unwrapping
+             "herra": unwrapping with Herra algorithm
+             "numpy": unwrapping with numpy algorithm
+
+       Returns:
+          phase (double): the phase as np array
+       """
        phi= 0.0
        if param == "raw":
           print( "get raw phase") 
@@ -358,49 +452,149 @@ class emf(object):
           print("numpy unwrap")
           phi0= np.angle(self.field)
           phi= np.unwrap(phi0)
+       else :   
+          print("error: unknown algorithm: ", param)
+
        return phi
   # getphase
 
    def getphotons(self):
-      "returns the photons"
+      """returns the photons
+
+      Returns:
+         photons (double): the number of photons
+      """
       return np.absolute(self.field)**2/377.0/1.6e-19
+   # getphotons
 
    def getreal(self):
-      "returns the real part of the field"
+      """returns the real part of the field
+
+      Returns:
+         field (double): the real part of the field as np array
+      """
       return self.field.real
+   # getreal
 
    def getWavelength( self ):  
-      "returns the wavelength in m"
+      """returns the wavelength in m
+
+      Returns:
+         wavelength (double): wavelength in m
+      """
       return self.wavelength
+   # getWavelength
 
    def gety_vec( self ):
-       "returns the y vector"
+       """returns the y vector
+
+       Returns:
+          y (double): vector
+       """
        return self.y_vec
+   # gety_vec
 
    def getz_vec( self ):
-       "returns the z vector"
+       """returns the z vector
+
+       Returns:
+          z (double): vector
+       """
        return self.z_vec
+   # getz_vec   
 
    def setField( self, field ):  
-      "set a new field"
+      """set a new field
+
+      Args:
+         field (complex): np complex array
+      """
       self.field= field
+   # setField
 
    def setName( self, name ):  
-      "set a new name"
+      """set a new name
+
+      Args:
+         name (string): name
+      """
       self.name= name
+   # setName
 
    def setWavelength( self, wavelength ):  
-      "set a new wavelength in m"
+      """set a new wavelength in m
+
+     Arg:
+        wavelength (double): wavelength in m
+      """
       self.wavelength= wavelength
+   # end setWavelength   
 
    def sety_vec( self, vec ):  
-      "set a new y_vec"
+      """set a new y_vec
+
+      Arg:
+         vec (double): vector as np array
+      """
+
       self.y_vec= vec
 
    def setz_vec( self, vec ):  
-      "set a new z_vec"
-      self.z_vec= vec
+      """set a new z_vec
 
+      Args:
+         vec (double): vector as np array
+      """
+
+      self.z_vec= vec
+      # end setz_vec
+
+   def torus(degree=False, s1=0.0, s2=0.0, thetan=None, thetag=None, verbose=True):
+      """calculate radii of a toroidal mirror- angle in rad or degree, grazing or normal
+      
+      Args:
+         degree=False (bool): theta input in degree, default is rad
+         s1=0.0 (double): source distance in m
+         s2=0.0 (double): image distance in m 
+         thetag=None (double): grazing angle
+         thetan=None (double): angle to normal
+         verbose=True (bool): verbosity
+      
+      Returns:
+         rw (double): radius in w in m  
+         rl (double: radius in l in m
+
+      Example:
+          >>> emf = phase.initphase()
+              ans = emf.torus(s1=10, s2=5, thetag=30, degree=True)
+              print(ans)
+      """
+      if (thetan != None) and (degree != None) :
+         thetan= thetan* np.pi/180. 
+      if (thetag != None) and (degree != None) :
+         thetan= np.pi/2.0 - thetag * np.pi/ 180.
+      if (thetag != None) and (degree is None) :
+         thetan= np.pi/2.0 - thetag
+      if (np.abs(thetan) >= np.pi/2.0) or (np.abs(s1) < 1e-10) or (np.abs(s2) < 1e-10) :
+         print("error: unphysical input- return")
+         return
+      
+      rw= 1.0/((1.0/s1+ 1.0/s2) * np.cos(thetan)/2.0)
+      rl= 1.0/((1.0/s1+ 1.0/s2) / (2.0 * np.cos(thetan)))
+      thetag= np.pi/2.0- thetan
+
+      if verbose :
+         print('=========== torus ==============')
+         print('s1    = {:10.4g} m'.format(s1))
+         print('s2    = {:10.4g} m'.format(s2))
+         print('thetan= {:7.4f} = {:7.4f} deg.'.format(thetan, thetan* 180/ np.pi))
+         print('thetag= {:7.4f} = {:7.4f} deg.'.format(thetag, thetag* 180/ np.pi))
+         print('rw    = {:10.4g} m'.format(rw))
+         print('rl    = {:10.4g} m'.format(rl))
+         print('========== torus end ===========')
+
+      return rw, rl   
+   # end torus    
 
 #    def instance_method(self,arg):
 #        statements
