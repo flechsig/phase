5f93cccd68515504db70ad2b7535d2f3ed87f477
Uwe Flechsig <uwe.flechsig@psi.ch>
2017-08-23 09:15:46 +0200

    new day

--- src/phasepython/phase.py
+++ src/phasepython/phase.py
@@ -1,6 +1,6 @@
 # File      : /afs/psi.ch/project/phase/GIT/phase/src/phasepython/phase.py
 # Date      : <15 Aug 17 16:25:49 flechsig> 
-# Time-stamp: <21 Aug 17 17:12:51 flechsig> 
+# Time-stamp: <22 Aug 17 16:02:11 flechsig> 
 # Author    : Uwe Flechsig, uwe.flechsig&#64;psi.&#99;&#104;
 
 # $Source$ 
@@ -35,8 +35,11 @@
 #
 # ******************************************************************************
 
+# the class methods are in alphabetical order
+
 import numpy as np
 import matplotlib.pyplot as plt
+import gausfitter2
 
 class emf(object):
    """main object of an electromagnetic field """
@@ -57,29 +60,134 @@ class emf(object):
 
         return s   
 
-   def aperture(type=1):
-      """acts as an aperture, or generates wavefield with amplitude according to 'type'
+   def aperture(self, example=False, shape=1, p1=None, p2=None, p3=None, p4=None, 
+                plot=False, Ny=None, sizey=None, Nz=None, sizez=None, verbose=True):
+      """acts as an aperture, or generates wavefield with amplitude according to 'shape'
 
       Args:
+         example=False (bool):
          field
          example
-         type=1 (int):      type of aperture
-            type 1  : rectangular              P1 = hsize, P2 = vsize
-            type 2  : vertical slit            P1 = hsize, P2 = hpos (default= 0)
-            type 3  : horizontal slit          P1 = vsize, P2 = vpos (default= 0)
-            type 12 : double slit vertical,    P1 = hsize, P2= hsep
-            type 13 : double slit horizontal,  P1 = vsize, P2= vsep
-            type 20 : circular                 P1 = Radius
+         shape=1 (int):      shape of aperture
+            shape 1  : rectangular              P1 = hsize, P2 = vsize
+            shape 2  : vertical slit            P1 = hsize, P2 = hpos (default= 0)
+            shape 3  : horizontal slit          P1 = vsize, P2 = vpos (default= 0)
+            shape 12 : double slit vertical,    P1 = hsize, P2= hsep
+            shape 13 : double slit horizontal,  P1 = vsize, P2= vsep
+            shape 20 : circular                 P1 = Radius
                       Radius > 0: central circular part is transparent
                       Radius < 0: central circular part is oblique
-            type 21 : annular                  P1 = Outer radius, P2 = inner radius
-            type 32 : vertical mirror          P1 = length, P2 = grazing angle (rad)
-            type 33 : horizontalal mirror      P1 = length, P2 = grazing angle (rad)
+            shape 21 : annular                  P1 = Outer radius, P2 = inner radius
+            shape 32 : vertical mirror          P1 = length, P2 = grazing angle (rad)
+            shape 33 : horizontalal mirror      P1 = length, P2 = grazing angle (rad)
+
+      Returns:
+         mask (float): the mask array as np array
 
       Example:
           >>> emf = phase.initphase()
-              emf.aperture(example=True)
+              mask= emf.aperture()
       """
+   
+      usage= 'usage: aperture, [emf,][field=field, y_vec=y_vec, z_vec=z_vec,] shape=shape, [P1=P1,] [P2=P2,] '
+      print('aperture called')
+
+      self.name = 'Aperture'
+
+      if example :
+         print('**********************************************************')
+         print('example: double slit ')
+         print('**********************************************************')
+         self.aperture(shape=12, p1=2e-3, p2=5e-3, Ny=51, sizey=2e-2, plot=True)
+         print('**********************************************************')
+         print('end example')
+         print('**********************************************************')
+         return
+     
+      if (Ny and sizey) or (Nz and sizez) :  # generate a field
+         create= True
+         print("generate a new field")
+         if (Ny and sizey) and not (Nz and sizez) :
+            Nz= Ny
+            sizez= sizey
+         if not (Ny and sizey) and (Nz and sizez) :
+            Ny= Nz
+            sizey= sizez
+         print("Ny= {:d}, Nz= {:d}, sizey= {:.3g} m, sizez= {:.3g} m".format(Ny, Nz, sizey, sizez))
+         self.z_vec = np.linspace(-0.5 * sizez, 0.5 * sizez, Nz)  
+         self.y_vec = np.linspace(-0.5 * sizey, 0.5 * sizey, Ny)
+         self.field = np.zeros((Ny, Nz), dtype=complex) + 1.0 + 0j   # complex array with 1.0 = 0j  
+      # end generate field 1 
+
+#;; call case twice for speed     
+      shapefound=False
+      if shape == 1 :         # rectangular
+         shapefound=True
+         if not p2 : 
+            p2 = p1
+         if not p3 :
+            h0= 0.0 
+         else : 
+            h0= p3
+         if not p4 :
+            v0= 0.0 
+         else : 
+            v0= p4
+         p1half= 0.5 * p1
+         p2half= 0.5 * p2
+         if verbose : 
+            print('rectangular aperture (h x v): ', p1, p2, ' offsets: ', h0, v0)
+      # end 1
+
+      if shape == 2 :         # vertical slit
+         shapefound=True
+         if not p2 : 
+            p2 = 0.0
+         p1half= 0.5 * p1
+         if verbose : 
+            print('vertical slit (hwidth, hpos): ', p1, p2)
+      # end 2
+
+      if shape == 12 :         # vertical double slit
+         shapefound=True
+         if not p2 : 
+            p2 = p1
+         p1half= 0.5 * p1
+         p2half= 0.5 * p2
+         if verbose : 
+            print('vertical double slit (hwidth, hpos): ', p1, p2)
+      # end 2
+
+      if not shapefound :
+         print("error: unknown shape, shape= ", shape, " => return")
+         return
+      #end shape definitions
+
+# fill
+      Ny= len(self.y_vec)
+      Nz= len(self.z_vec)
+      T= np.zeros((Ny, Nz), dtype=float)
+      for row in np.arange(Ny) :
+         for col in np.arange(Nz) :
+            if shape == 1 :
+               if ((np.abs(self.z_vec[col]- h0) <= p1half) and (np.abs(self.y_vec[row]- v0) <= p2half)) :
+                  T[row, col]= 1.0
+            if shape == 2 :
+               if np.abs(self.z_vec[col]- p2) <= p1half :
+                  T[row, col]= 1.0
+            if shape == 12 :
+               if (np.abs(self.z_vec[col]) <= (p2half + p1half)) and (np.abs(self.z_vec[col]) >= (p2half - p1half)) :
+                  T[row, col]= 1.0
+      # 2d for loop
+      
+      #print("T: ", T)
+      #self.mycontour(T, self.z_vec, self.y_vec)
+      self.field*= T      
+      return T                                                    
+   """   self.wavelength= wavelength
+      self.field = field
+      self.z_vec = z_vec
+      self.y_vec = y_vec"""
    # end aperture
 
    def gaussbeam(self, dist=None, drift=None, w0=1e-5, Nz=243, Ny=None, sizez=1e-3, sizey=None, \
@@ -103,7 +211,7 @@ class emf(object):
 
        Example:
           >>> emf = phase.initphase()
-              emf.gaussbeam(example=True)   
+              emf.gaussbeam(example=True, plot=True)   
        """
        self.name = 'Gaussbeam'
 
@@ -112,24 +220,24 @@ class emf(object):
            print('example: HeNe Laser ')
            print('wavelength=633e-9, w0= 1e-3, dist= 10., sizez=1e-2')
            print('**********************************************************')
-           self.gaussbeam(dist=10., wavelength=633e-9, w0=1e-3, sizez=1e-2, plot=True)
+           self.gaussbeam(dist=10., wavelength=633e-9, w0=1e-3, sizez=1e-2)
            print('**********************************************************')
            print('end example')
            print('**********************************************************')
            return
    
        if drift is not None:
-           dist= drift
+          dist= drift
        if Ny is None:
-           Ny= Nz
+          Ny= Nz
        if sizey is None:
-           sizey= sizez
+          sizey= sizez
 
-       field  = np.zeros((Nz, Ny), dtype=complex) 
-       z_vec  = np.linspace(-0.5*sizez, 0.5*sizez, Nz)  
-       y_vec  = np.linspace(-0.5*sizey, 0.5*sizey, Ny)
-       dz     = z_vec[1]- z_vec[0]
-       dy     = y_vec[1]- y_vec[0]
+       field = np.zeros((Ny, Nz), dtype=complex) 
+       z_vec = np.linspace(-0.5*sizez, 0.5*sizez, Nz)  
+       y_vec = np.linspace(-0.5*sizey, 0.5*sizey, Ny)
+       dz    = z_vec[1]- z_vec[0]
+       dy    = y_vec[1]- y_vec[0]
 
        print( 'wavelength (m) = ', wavelength)
        print( 'Nz     = ', Nz      , ', Ny     = ', Ny)
@@ -199,14 +307,23 @@ class emf(object):
            #endif  plot
 
        self.wavelength= wavelength
-       self.field= field
-       self.z_vec= z_vec
-       self.y_vec= y_vec
+       self.field = field
+       self.z_vec = z_vec
+       self.y_vec = y_vec
+   # end gaussbeam
 
-   def mycontour(self, z, x, y, xlabel='z (mm)', ylabel='y (mm)', zlabel='intensity (W)', title='title'): 
+   def mycontour(self, z, x, y, xlabel='z (mm)', ylabel='y (mm)', zlabel='intensity (W)', title='title', figure=True): 
        """make a contour plot (helper function)
 
        Args:
+          z (double): a 2d array
+          x (double): vector in horizontal direction
+          y (double): vector in vertical direction
+          figure=True (bool): plot a new figure
+          title='title' (string): title
+          xlabel='z (mm)' (string): label
+          ylabel='y (mm)' (string): label
+          zlabel='intensity (W)' (string): label
        
        Example:
           >>> emf = phase.initphase()
@@ -215,6 +332,8 @@ class emf(object):
               emf.mycontour(ph)
 
        """
+       if figure is True:
+          plt.figure()
        cp= plt.contour(x, y, z, 15, linewidths = 0.5, colors = 'k')  # contour linien
        plt.pcolormesh(x, y, z, cmap = plt.get_cmap('rainbow'))       # farbe
        cbar= plt.colorbar() 
@@ -228,31 +347,55 @@ class emf(object):
        # mycontour
 
    def plotamplitude( self ):
-      """plot the amplitude"""
+      """plot the amplitude
+      
+      Example:
+          >>> emf = phase.initphase()
+              emf.gaussbeam(example=True)
+              emf.plotamplitude()
+      """
       
       title= self.getname()+ " amplitude"
       self.mycontour(self.getamplitude(), self.getz_vec() * 1e3, self.gety_vec() * 1e3, zlabel= 'amplitude ($V/m^3$)', title=title)
    # end plotamplitude 
  
    def plotimag( self ):
-      """plot the imaginary part of the field"""
+      """plot the imaginary part of the field
+
+      Example:
+          >>> emf = phase.initphase()
+              emf.gaussbeam(example=True)
+              emf.plotimag()
+      """
       
       title= self.getname()+ " imaginary part"
-      self.mycontour(self.getimag(), self.getz_vec() * 1e3, self.gety_vec() * 1e3, zlabel= 'imaginary part ()', title=title)
+      self.mycontour(self.getimag(), self.getz_vec() * 1e3, self.gety_vec() * 1e3, zlabel= 'imaginary part (a. u.)', title=title)
    # end plotimag
 
    def plotphotons( self ):
-      """plot the field intensity as photons"""
+      """plot the field intensity as photons
+
+      Example:
+          >>> emf = phase.initphase()
+              emf.gaussbeam(example=True)
+              emf.plotphotons()
+      """
       
       title= self.getname()+ " photons"
-      self.mycontour(self.getphotons(), self.getz_vec() * 1e3, self.gety_vec() * 1e3, zlabel= 'photon density ($1/m^2$)', title=title)
+      self.mycontour(self.getphotons(), self.getz_vec() * 1e3, self.gety_vec() * 1e3, zlabel= 'photon density ($1photons/s m^2$)', title=title)
    # end plotphotons
 
    def plotreal( self ):
-      """plot the real partof the field"""
+      """plot the real part of the field
+
+      Example:
+          >>> emf = phase.initphase()
+              emf.gaussbeam(example=True)
+              emf.plotreal()
+      """
       
       title= self.getname()+ " real part"
-      self.mycontour(self.getreal(), self.getz_vec() * 1e3, self.gety_vec() * 1e3, zlabel= 'real part ()', title=title)
+      self.mycontour(self.getreal(), self.getz_vec() * 1e3, self.gety_vec() * 1e3, zlabel= 'real part (a. u.)', title=title)
    # end plotreal  
    
    def propfraunhofer(self, drift=None):
@@ -526,6 +669,11 @@ class emf(object):
 
      Arg:
         wavelength (double): wavelength in m
+
+        Example:
+          >>> emf = phase.initphase()
+              emf.setWavelength(1e-10)
+              emf.getWavelength()
       """
       self.wavelength= wavelength
    # end setWavelength   
@@ -535,48 +683,177 @@ class emf(object):
 
       Arg:
          vec (double): vector as np array
+
+      Example:
+          >>> emf = phase.initphase()
+              emf.sety_vec(np.arange(11) - 5.0)
+              emf.gety_vec()
       """
 
       self.y_vec= vec
+   # end sety_vec
 
    def setz_vec( self, vec ):  
       """set a new z_vec
 
       Args:
          vec (double): vector as np array
+
+      Example:
+          >>> emf = phase.initphase()
+              emf.setz_vec(np.arange(11) - 5.0)
+              emf.getz_vec()
       """
 
       self.z_vec= vec
       # end setz_vec
 
-   def torus(degree=False, s1=0.0, s2=0.0, thetan=None, thetag=None, verbose=True):
+   def statistics(self, comment=None, amplitude=False, nofit=False):
+      """shows statistics 
+
+      Args:
+         comment=None (string): comment string (optional)
+         amplitude=False (bool): amplitude statistics - default: intensity
+         nofit=False (bool): skip 2d fit
+      
+     Returns:
+          stat (dictionary)
+
+      Example:
+          >>> emf = phase.initphase()
+              emf.gaussbeam(example=True)
+              emf.statistics()
+      """
+
+      if amplitude :
+         title= "amplitude statistics"
+         myfield= self.getamplitude()
+         mymaxstr= 'max field       (V/m) = ' 
+         mytotstr= 'total field     (V m) = '
+      else :
+         title= "intensity statistics"
+         myfield= self.getintensity()
+         mymaxstr= 'max intensity ($W/m^2$) = ' 
+         mytotstr= 'total intensity (W)   = '
+
+      if comment :
+         title+= ' => ' + comment
+
+      z_vec= self.z_vec
+      y_vec= self.y_vec
+      wavelength= self.wavelength
+      zmin= np.amin(z_vec)
+      zmax= np.amax(z_vec)
+      ymin= np.amin(y_vec)
+      ymax= np.amax(y_vec)
+      binsize= (z_vec[1]- z_vec[0]) * (y_vec[1]- y_vec[0])
+
+      mymax= np.amax(myfield)                        # photons/m^2
+      mymaxidx= np.unravel_index(myfield.argmax(), myfield.shape)
+      mysum= np.sum(myfield)
+      mytot= np.sum(myfield) * binsize   # sum of all bins*binsize
+
+      if mymax > 0.0 :
+         field_n= myfield/mymax
+      else :
+         field_n= myfield     # normalized field for fit
+
+ ##    stat   = dblarr(7)
+
+      if not nofit :
+         ##fit= ##gauss2dfit(field_n, stat, z_vec, y_vec) 
+         stat= gausfitter2.gaussfit(field_n)
+      else :
+         print('we do not fit- we search fwhm')
+         z0i= mymaxidx[1]
+         y0i= mymaxidx[0]
+         mymax05= 0.5 * mymax
+#;    print('z0i,y0i,mymax05',z0i,y0i,mymax05
+         zcut= myfield[:,y0i]
+         ycut= myfield[z0i,:]
+#;    plot, ycut
+#;    print( 'zcut::::', zcut
+         zidx= (zcut > mymax05).nonzero()
+#;    print( 'zidx::::', zidx
+# ;   help, zidx
+         yidx= (ycut > mymax05).nonzero()
+    
+         if len(zidx) > 1 :
+            zfwhm= z_vec[zidx[len(zidx)-1]]- z_vec[zidx[0]]
+         else :
+            zfwhm= 0
+         if len(yidx) > 1 :
+            yfwhm= y_vec[yidx[len(yidx)-1]]- y_vec[yidx[0]]
+         else :
+            yfwhm= 0
+
+
+      print('==============================================================================')
+      print( title )
+      print('==============================================================================')
+      if not nofit :
+         print('z fwhm = {:7.5g} m, rms = {:.5g} m'.format(stat[2] * 2.35, stat[2]))
+         print('y fwhm = {:7.5g} m, rms = {:.5g} m'.format(stat[3] * 2.35, stat[3]))
+         print('z0     = {:7.5g} m'.format(stat[4]))
+         print('y0     = {:7.5g} m'.format(stat[5]))
+      else :
+         print('z fwhm=', zfwhm, ' m')
+         print( 'y fwhm=', yfwhm, ' m')
+         print( 'z0    =', z_vec[z0i], ' m')
+         print( 'y0    =', y_vec[y0i], ' m')
+
+      print( 'zmin, zmax (m) =', zmin, zmax, ', nz=', len(z_vec))
+      print( 'ymin, ymax (m) =', ymin, ymax, ', ny=', len(y_vec))
+      print('wavelength (nm)= {:.3f}'.format(wavelength * 1e9))
+      print( mymaxstr, mymax)
+      print( mytotstr, mytot)
+      if not amplitude:
+         print('max intensity (photons/m^2) = ', mymax/(1.6e-19*1240e-9/self.wavelength))
+         print('total intensity (photons)   = ', mytot/(1.6e-19*1240e-9/self.wavelength))
+
+      print('debug: mysum, binsize=', mysum, binsize)
+      print( '==============================================================================')
+      if not nofit :
+         print( 'result of gauss2dfit in (m):', stat)
+         print( '==============================================================================')
+
+
+      total=mytot
+      max= mymax
+
+
+   # end statistics
+
+   def torus(self, degree=False, s1=10.0, s2=5.0, thetan=0.0, thetag=None, verbose=True):
       """calculate radii of a toroidal mirror- angle in rad or degree, grazing or normal
       
       Args:
          degree=False (bool): theta input in degree, default is rad
-         s1=0.0 (double): source distance in m
-         s2=0.0 (double): image distance in m 
+         s1=10.0 (double): source distance in m
+         s2=5.0 (double): image distance in m 
          thetag=None (double): grazing angle
-         thetan=None (double): angle to normal
+         thetan=0.0 (double): angle to normal
          verbose=True (bool): verbosity
       
       Returns:
-         rw (double): radius in w in m  
-         rl (double: radius in l in m
+         rw, rl
+            rw (double): radius in w in m  
+            rl (double: radius in l in m
 
       Example:
           >>> emf = phase.initphase()
-              ans = emf.torus(s1=10, s2=5, thetag=30, degree=True)
+              ans = emf.torus(s1=10, s2=5, thetan= 30, degree=True)
               print(ans)
       """
-      if (thetan != None) and (degree != None) :
-         thetan= thetan* np.pi/180. 
-      if (thetag != None) and (degree != None) :
-         thetan= np.pi/2.0 - thetag * np.pi/ 180.
-      if (thetag != None) and (degree is None) :
-         thetan= np.pi/2.0 - thetag
+      if thetan and degree :
+         thetan = thetan * np.pi/180. 
+      if thetag and degree :
+         thetan = np.pi/2.0 - thetag * np.pi/ 180.
+      if thetag and not degree :
+         thetan = np.pi/2.0 - thetag
       if (np.abs(thetan) >= np.pi/2.0) or (np.abs(s1) < 1e-10) or (np.abs(s2) < 1e-10) :
          print("error: unphysical input- return")
+         print("usage example: ans = emf.torus(s1=10, s2=5, thetag=1e-3)")
          return
       
       rw= 1.0/((1.0/s1+ 1.0/s2) * np.cos(thetan)/2.0)
@@ -591,6 +868,7 @@ class emf(object):
          print('thetag= {:7.4f} = {:7.4f} deg.'.format(thetag, thetag* 180/ np.pi))
          print('rw    = {:10.4g} m'.format(rw))
          print('rl    = {:10.4g} m'.format(rl))
+         print('M     = {:10.4g} : 1'.format(s1/s2))
          print('========== torus end ===========')
 
       return rw, rl   
