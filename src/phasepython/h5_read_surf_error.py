3322be248a4a81829360f63706921636fc32fdf4
flechsig <uwe.flechsig@psi.ch>
2018-03-01 15:24:10 +0100

    xx

--- src/phasepython/h5_read_surf_error.py
+++ src/phasepython/h5_read_surf_error.py
@@ -1,31 +1,48 @@
 import h5py
 import numpy as np
+import PyQt4.QtGui
 
-def h5_read_surf_error(fname, verbose=True) :
+def print_attrs(name, obj):
+   print("name= ", name)
+
+def h5_read_surf_error(fname=None, ename=None, verbose=True) :
    """h5_write_surf_error - write surface error into hdf5
 
    Args:
+      fname (string): filename
+      ename (string): elementname
       verbose=True (bool): verbosity
 
    Returns:
       dictionary
 
    Example:
-          >>> dict= h5_read_surf_error('surf_err.h5') 
+          >>> dict= h5_read_surf_error('surf_err.h5', 'M11') 
               
 
    """
 
-   usage= "usage: h5_read_surf_error('surf_err.h5', 'ename', u, w, l)"
-   print("Usage: ", usage) 
+#   usage= "usage: h5_read_surf_error('surf_err.h5', 'ename')"
+#   print("Usage: ", usage) 
 
-
+   if (fname == None):
+      fname= PyQt4.QtGui.QFileDialog.getOpenFileName(None, 'Open file', '.', 
+                                                      "hdf5 files (*.h5);;All files (*.*)")
 
    f= h5py.File(fname)
-   u2= f['/height2D'][:]
-   u1= f['/height_vec'][:]
-   w= f['/wvec'][:]
-   l= f['/lvec'][:]
+
+   if (ename == None):
+# List all groups
+      print("no ename provided - list available keys")
+#      print("Keys: %s" % f.keys()
+      #a_group_key = list(f.keys())[0]
+      f.visititems(print_attrs)
+      return
+
+   u2= f[ename+'/height2D'][:]
+   u1= f[ename+'/height_vec'][:]
+   w= f[ename+'/wvec'][:]
+   l= f[ename+'/lvec'][:]
    f.close()
    print('read surface error file: ', fname)
 
